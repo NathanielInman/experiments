aaf8509b4396bd7664498872086e007fe45f1587
Nate <nate@codekoalas.com>
2014-10-03 18:11:49 -0500

    Removed test.sh bash script.

--- test.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter \
-  'if [ $GIT_COMMIT = 43347c97f2a149fed8d16f35ae85925ba1ca6522 ]
-   then
-     export GIT_AUTHOR_DATE="Sat Oct 11 21:38:53 2014 -0600"
-     export GIT_COMMITTER_DATE="Sat Oct 11 22:01:01 2014 -0600"
-   fi'
