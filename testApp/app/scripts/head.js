d767c03faae29b13fd63094cd91fe31b30138f02
Nate <nate@codekoalas.com>
2014-10-13 18:13:21 -0500

    Cleaned root directory of experiments repo.

--- testApp/app/scripts/head.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Head.JS
- * P.S. Included all scripts, that necessary to place at the head tag, except some specific old IE polyfills
- * Author: Nathaniel Inman
- * Email: nate041010@gmail.com
- */
-
-
