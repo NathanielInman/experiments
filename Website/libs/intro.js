88671ca5b7d82e4c95e3978747c3eb6a11f58063
Nate <nate@codekoalas.com>
2014-12-21 14:11:23 -0600

    ion now allows for using images for particles instead of just a pixel.

--- Website/libs/intro.js
+++ Website/libs/intro.js
@@ -1,3 +1,24 @@
-/**
- * Created by nate on 12/21/14.
- */
+var smokeImage = new Image();
+    smokeImage.src="imgs/smokeParticle.png";
+var waterfall            = new Ion(1000);
+waterfall.sx         = function(){return v.w/2;};
+waterfall.sy         = 0;
+waterfall.dx         = function(){return v.w/2;};
+waterfall.dy         = function(){return v.h;};
+waterfall.wx         = function(){return r(0,3)-1.5;};
+waterfall.wy         = 0;
+waterfall.size       = function(){return r(1,3);};
+waterfall.tween_type = 35;
+waterfall.image      = smokeImage;
+waterfall.imageWidth = 256;
+waterfall.imageHeight= 256;
+waterfall.onEscape   = function(id){this.onEnd(id);};
+waterfall.onEnd      = function(id){
+  this.particle[id].x  = this.particle[id].sx=v.w/2;
+  this.particle[id].y  = this.particle[id].sy=0;
+  this.particle[id].dx = v.w/2;
+  this.particle[id].dy = v.h;
+  this.particle[id].c  = 0;
+};
+waterfall.populate();
+waterfall.process();
