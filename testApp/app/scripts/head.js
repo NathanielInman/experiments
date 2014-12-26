0070ed150e75ee07d669e2a728e21350af5cb4d0
Nate <nate@codekoalas.com>
2014-12-26 15:45:13 -0600

    Added testApp.

---
+++ testApp/app/scripts/head.js
@@ -0,0 +1,8 @@
+/*!
+ * Head.JS
+ * P.S. Included all scripts, that necessary to place at the head tag, except some specific old IE polyfills
+ * Author: Nathaniel Inman
+ * Email: nate041010@gmail.com
+ */
+
+
