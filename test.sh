3cdc674114bc615d0bf3bb7b3c918596ca1020b7
Nate <nate@codekoalas.com>
2015-01-19 18:13:32 -0600

    Adjusted test.sh.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 git filter-branch --env-filter \
   'if [ $GIT_COMMIT = f6e57816882077498c2d069313f22c8cc6aa1c20 ]
    then
-     export GIT_AUTHOR_DATE="Tue Oct 7 21:38:53 2014 -0600"
-     export GIT_COMMITTER_DATE="Tue Oct 7 22:01:01 2014 -0600"
+     export GIT_AUTHOR_DATE="Wed Oct 8 21:38:53 2014 -0600"
+     export GIT_COMMITTER_DATE="Wed Oct 8 22:01:01 2014 -0600"
    fi'
