36d213a97957b55871d014ec5e2e8c8b60417433
Nathaniel Inman <nate041010@gmail.com>
2015-04-07 15:35:09 -0500

    When changing from one environment to another while a map is already created, it adjusts the wall types and floor types of the currently created sectors to ensure that they're all appropriate from the new environment type. Lava isn't allowed in swamp environments for example.

--- Map Generation/mapMaker/src/scripts/engine/controllers/input/environmentClick.js
+++ Map Generation/mapMaker/src/scripts/engine/controllers/input/environmentClick.js
@@ -1,20 +1,40 @@
-import { map } from 'engine/data-model/map';
-import { draw } from 'engine/controllers/draw/main';
-import { hex2rgba } from 'engine/controllers/draw/hex2rgba';
+import { map         } from 'engine/data-model/map';
+import { draw        } from 'engine/controllers/draw/main';
+import { hex2rgba    } from 'engine/controllers/draw/hex2rgba';
+import { environment } from 'engine/data-model/environment';
 
 export function environmentListClick(e){
-  console.log('a',e)
   if(e.target.hash){
-      map.setEnvironment(e.target.hash.replace('#',''));
-      $(this).find('.selected').removeClass('selected');
-      $(e.target).addClass('selected');
-      $('#environments').hide();
+    // First start off by seeing if the new index is different from
+    // the old index. If it is, then we need to iterate through all of
+    // the created sectors re-appropriate new wall and floors that
+    // are correct for the new environment. This is required because
+    // not all environments allow all floor and wall types; such as a
+    // volcanoe wouldn't allow a pool of water, and a pond wouldn't allow
+    // a lavaflow sector.
+    let newIndex = e.target.hash.replace('#','');
+    let oldIndex = map.getEnvironmentIndex();
+    if(newIndex !== oldIndex){
+      let newFloors = environment[newIndex].floor;
+      let newWalls = environment[newIndex].wall;
+      for(let index in map.sector){
+        map.sector[index].floor = newFloors[r(0,newFloors.length,1)];
+        map.sector[index].wall = newWalls[r(0,newWalls.length,1)];
+      } //end for
+    } //end if
+
+    // Remove the previously selected environment if it exists, and update
+    // the environment to the new one, while hiding the dropdown list now
+    // that an environment was selected.
+    map.setEnvironment(newIndex);
+    $(this).find('.selected').removeClass('selected');
+    $(e.target).addClass('selected');
+    $('#environments').hide();
   } //end if
   draw();
 }
 
 export function environmentBtnClick(e){
-  console.log('b',e)
   if($('#environments').is(':visible')){
     $('#environments').hide();
   }else{
@@ -23,32 +43,25 @@ export function environmentBtnClick(e){
 }
 
 export function environmentBtnDown(e){
-  let d = true;
   let comboButton = $('#changeEnvironment');
   let c = map.getEnvironment().background.value;
-  var s1 = 1.5,s2=1,s3=0.6;
-  if(d){ s1*=0.9;s2*=0.9;s3*=0.9; } //if we're being pressed, lower the shading levels
+  var s1 = 1.35,s2=0.9,s3=0.54;
   comboButton.css('background','-webkit-linear-gradient(-90deg,'+hex2rgba(c,{r:s1,g:s1,b:s1},0.5)+','+hex2rgba(c,{r:s2,g:s2,b:s2},0.5)+' 30%,'+hex2rgba(c,{r:s3,g:s3,b:s3},0.5)+' 50%)');
   comboButton.css('color','#000');
-  console.log(c);
 }
 
 export function environmentBtnOver(e){
-  let v = true;
   let comboButton = $('#changeEnvironment');
   let c = map.getEnvironment().background.value;
-  var s1 = 1.5,s2=1,s3=0.6;
-  if(v){ s1*=1.2;s2*=1.2;s3*=1.2; } //if we're hovering, raise the shading levels
+  let s1 = 1.8,s2=1.2,s3=0.72;
   comboButton.css('background','-webkit-linear-gradient(-90deg,'+hex2rgba(c,{r:s1,g:s1,b:s1},0.5)+','+hex2rgba(c,{r:s2,g:s2,b:s2},0.5)+' 30%,'+hex2rgba(c,{r:s3,g:s3,b:s3},0.5)+' 50%)');
   comboButton.css('color','#000');
-  console.log(c);
 }
 
 export function environmentBtnOut(e){
   let comboButton = $('#changeEnvironment');
   let c = map.getEnvironment().background.value;
-  var s1 = 1.5,s2=1,s3=0.6;
+  let s1 = 1.5,s2=1,s3=0.6;
   comboButton.css('background','-webkit-linear-gradient(-90deg,'+hex2rgba(c,{r:s1,g:s1,b:s1},0.5)+','+hex2rgba(c,{r:s2,g:s2,b:s2},0.5)+' 30%,'+hex2rgba(c,{r:s3,g:s3,b:s3},0.5)+' 50%)');
   comboButton.css('color','#000');
-  console.log(c);
 }
