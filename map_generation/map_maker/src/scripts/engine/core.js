12030783e9d19dd644fb5e0f0c43229be70e289f
Nathaniel Inman <nate041010@gmail.com>
2015-04-13 12:56:05 -0500

    Creating a new way of loading data. We'll pass in an onReady function to the engine so it can call that once all the data is loaded. We'll have links to all the data assets that will be fetched and cached prior to the engine being loaded.

--- map_generation/map_maker/src/scripts/engine/core.js
+++ map_generation/map_maker/src/scripts/engine/core.js
@@ -10,14 +10,12 @@ import { environmentBtnClick  } from 'engine/controllers/input/environmentClick'
 import { environmentBtnDown   } from 'engine/controllers/input/environmentClick';
 import { environmentBtnOver   } from 'engine/controllers/input/environmentClick';
 import { environmentBtnOut    } from 'engine/controllers/input/environmentClick';
-import { map                  } from 'engine/data-model/map';
 import { creature             } from 'engine/data-model/creature';
 import { environment          } from 'engine/data-model/environment';
 import { floor                } from 'engine/data-model/floor';
 import { wall                 } from 'engine/data-model/wall';
 import { components           } from 'engine/data-model/components';
 
-console.log('core');
 var Engine = function(){};
 Engine.prototype.draw = draw;
 Engine.prototype.input = {};
@@ -31,13 +29,25 @@ Engine.prototype.input.environmentBtnClick = environmentBtnClick;
 Engine.prototype.input.environmentBtnDown = environmentBtnDown;
 Engine.prototype.input.environmentBtnOver = environmentBtnOver;
 Engine.prototype.input.environmentBtnOut = environmentBtnOut;
-Engine.prototype.map = map;
 Engine.prototype.database = {};
 Engine.prototype.database.creature = creature;
 Engine.prototype.database.environment = environment;
+$.ajax('http://localhost:5000/world/environments/')
+  .done(function(res,status){
+    console.log('ENGINE!! after ajax');
+    if(status=='success'){
+      obj = res;
+      console.log('ENGINE !!',res);
+      engine.database.environment = res;
+      engine.initialize();
+      Easel.redraw();
+    }else{
+      console.log(arguments);
+    } //end if
 Engine.prototype.database.floor = floor;
 Engine.prototype.database.wall = wall;
-Engine.prototype.initialize = function(){
+Engine.prototype.onReady = function(){
+  console.log('on-ready of engine called');
   map = this.map = new Map(); //make sure to change the engine link and the actual map location
   this.draw();
 } //end if
--- map_generation/map_maker/src/scripts/engine/data-model/map.js
+++
@@ -1,4 +0,0 @@
-import { Map } from 'engine/object-model/Map';
-import { jQuery } from 'vendor/jquery-2.1.3.min';
-
-export var map = 'loading';
