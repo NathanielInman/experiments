5ed50f127226bf5944858d5bff2c810b0e3c475f
Nathaniel Inman <nate041010@gmail.com>
2015-04-30 14:30:05 -0500

    Re-implemented basic_mobiles experiment in ES6. It's a lot cleaner and faster.

--- map_generation/asynchronous/src/scripts/common/Map.js
+++ map_generation/asynchronous/src/scripts/common/Map.js
@@ -1,13 +1,5 @@
 /* global ctx */
 /* global v */
-class Sector{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.wall =  r(1)<0.25?1:0;
-    this.floor = r(1)<0.90?1:0; 
-  }
-}
 class Map{
   constructor(){
     this.width=3000;
@@ -24,20 +16,20 @@ class Map{
       } //end for
     } //end for
   }
-  
+
   /**
-   * We draw the map asynchronously, only updating as much sectors as we 
-   * possibly can within the specified Map fps. 
-   * 
+   * We draw the map asynchronously, only updating as much sectors as we
+   * possibly can within the specified Map fps.
+   *
    * k = the current sector being drawn
    * m = the max allowable sectors to be drawn per frame refresh.
-   *     this is computed dynamically per iteration 
+   *     this is computed dynamically per iteration
    * t = the time for which is current frame started so we can adjust
    *     the m variable each iteration until we maximize the drawn sectors
    *     to it's allowable fps threshold
    */
   draw(k,m,t){
-    var sector, //temp variable used to shorten and clean up code 
+    var sector, //temp variable used to shorten and clean up code
         i, j, // x && y variables computed based off the iteration number (k)
         w = this.width, h = this.height, //shorten width and height variables
         s = w*h,  //the max size is width x height
@@ -48,18 +40,18 @@ class Map{
       i=k%w; // Acquire the current horizontal position (x)
       j=k/w|0; // Acquire the current vertical position (y)
       sector = this.sector[i][j]; // make sector easier to write
-      if(sector.floor){ 
+      if(sector.floor){
         ctx.fillStyle='#232';
-      }else{ 
+      }else{
         ctx.fillStyle='#888';
       } //end if
       ctx.fillRect(i*v.w/w,j*v.h/h,v.w/w+1,v.h/h+1);
-      if(sector.wall){ 
+      if(sector.wall){
         ctx.fillStyle='#888';
         ctx.fillRect(i*v.w/w+v.w/w*0.1,j*v.h/h+v.h/h*0.1,v.w/w*0.8,v.h/h*0.8);
       } //end if
     } //end for
-    
+
     // If our current iteration is less then the size, there's more to do
     if(k<s){
       setTimeout(()=>{
---
+++ map_generation/basic_mobiles/src/scripts/common/directions.js
@@ -0,0 +1,4 @@
+const north = 0;
+const south = 1;
+const east  = 2;
+const west  = 3;
