e35e52b06a8a220766823eb8fafd349294b47242
Nathaniel Inman <nate041010@gmail.com>
2015-05-28 14:48:40 -0500

    Added the rest of the font files and got it working slightly.

--- map_generation/bsp/src/app.js
+++ map_generation/bsp/src/app.js
@@ -1,23 +1,18 @@
 import { easel } from "lib/easel";
 
-let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'fonts/icomoon.woff';
-let load = new Image();
-    load.src = 'fonts/icomoon.woff';
-    load.onload = function(){
-      alert('hm');
-    };
-document.getElementsByTagName('head')[0].appendChild(link);
 setTimeout(function(){
+ctx.font = '62px "icomoon"'
   ctx.fillStyle='red';
-  ctx.font = '62px "icomoon"';
   ctx.fillText('\ue617',200,200);
 },300);
 
 setTimeout(function(){
+ctx.font = '62px "icomoon"'
   ctx.fillStyle='blue';
-  ctx.fillText('\ue617',500,500);
-},401);
+  ctx.fillText('\ue617',300,200);
+},1401);
+setTimeout(function(){
+  ctx.fillStyle='green';
+  ctx.fillText('\ue617',400,200);
+},1800);
 console.log('Application started');
