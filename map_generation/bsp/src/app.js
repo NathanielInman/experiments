0a495df7731ca903a4ed53222d3856061e1dc05f
Nathaniel Inman <nate041010@gmail.com>
2015-06-03 07:33:45 -0500

    Re-began working on bsp experiment.

--- map_generation/bsp/src/app.js
+++ map_generation/bsp/src/app.js
@@ -1,40 +1,18 @@
-import { easel } from "lib/easel";
+import { easel } from 'lib/easel';
+import { Map   } from 'object-model/Map';
+import { bsp   } from 'controllers/bsp';
 
-var headerText = '  Version 0.1.0 by Nathaniel Inman';
-
-easel.config = function(){
-  ctx.textAlign = 'center';
-  ctx.font = '24px Impact';
-};
+// Create the instance of the map and transform it
+var map = new Map(50,50);
+bsp(map);
 
 easel.redraw = function(){
   // clear screen
   ctx.fillStyle='black';
   ctx.fillRect(0,0,v.w,v.h);
 
-  // draw jspm logo
-  ctx.strokeStyle='#959595';
-  ctx.fillStyle='#EDDA8D';
-  ctx.lineWidth=20;
-  ctx.lineCap='round';
-  ctx.moveTo(v.w/2,v.h/2+v.h/4);
-  ctx.lineTo(v.w/2-v.w/8,v.h/2+v.h/8);
-  ctx.lineTo(v.w/2-v.w/8,v.h/2-v.h/8);
-  ctx.lineTo(v.w/2,v.h/2-v.h/4);
-  ctx.lineTo(v.w/2+v.w/8,v.h/2-v.h/8);
-  ctx.lineTo(v.w/2+v.w/8,v.h/2+v.h/8);
-  ctx.lineTo(v.w/2,v.h/2+v.h/4);
-  ctx.moveTo(v.w/2,v.h/2);
-  ctx.lineTo(v.w/2-v.w/8,v.h/2-v.h/8);
-  ctx.moveTo(v.w/2,v.h/2);
-  ctx.lineTo(v.w/2+v.w/8,v.h/2-v.h/8);
-  ctx.moveTo(v.w/2,v.h/2);
-  ctx.lineTo(v.w/2,v.h/2+v.h/4);
-  ctx.fill();
-  ctx.stroke();
-
-  // Draw developer name and app version
-  ctx.fillText(headerText, v.w/2, v.h-5);
+  // refresh drawing the map when window requires it
+  map.redraw();
 };
 
 easel.config(); // this is called every time window resizes
---
+++ map_generation/bsp/src/object-model/Map.js
@@ -0,0 +1,6 @@
+export class Map{
+  constructor(w,h){
+    this.width=w||10;
+    this.height=h||10;
+  }
+};
