8aa866637d154abd9d5290207e9cd2c92315d383
Nathaniel Inman <nate041010@gmail.com>
2015-10-09 17:53:26 -0500

    fun project.

--- complex_math/Mandelbrot.js
+++ complex_math/Mandelbrot.js
@@ -1,42 +1,37 @@
-var v,w=function(){var e=window,a='inner';if(!(e.innerWidth)){a='client';e=document.documentElement||document.body;}return {w:e[a+'Width'],h:e[a+'Height']};},v=w();
-var $=function(o){return document.getElementById(o);};
-var sa=function(o,a){for(var i=0;i<a.length;i++){$(o).setAttribute(a[i][0],a[i][1]);}};
-var ctx,r=function(){ctx=$('canvas').getContext("2d");},q=function(){sa('canvas',[['width',v.w+'px'],['height',v.h+'px']]);};q();r();
-window.onresize=function(){v=w();r();q();ctx.fillStyle='#000';ctx.fillRect(0,0,v.w,v.h);};
-var ds=function(x,y,c){ctx.fillStyle=c;ctx.fillRect(x*v.w/size,y*v.h/size,v.w/size,v.h/size);};
-var random=function(n,p){return (p||0)+Math.random()*n|0;};
-ctx.fillStyle='#000';ctx.fillRect(0,0,v.w,v.h);ctx.fillStyle="#161";
-var xmin=-2,xmax=1,ymin=-1,ymax=1,iterations=1000;
-var Mandeliter=function(cx,cy,maxiter){
-	var x=0.0,y=0.0,tmp=0;
-	for(var i=0;i<maxiter&&x*x+y*y<=4;++i){
-		tmp=2*x*y;
-		x=x*x-y*y+cx;
-		y=tmp+cy;
-	} //end for
-	return i;
+var v,w=function(){var t=window,n="inner";return t.innerWidth||(n="client",t=document.documentElement||document.body),{w:t[n+"Width"],h:t[n+"Height"]}},v=w(),$=function(t){return document.getElementsByTagName(t)[0]},sa=function(t,n){for(var e=0;e<n.length;e++)$(t).setAttribute(n[e][0],n[e][1])},ctx,r=function(){ctx=$("canvas").getContext("2d")},q=function(){sa("canvas",[["width",v.w+"px"],["height",v.h+"px"]])};q(),r(),window.onresize=function(){v=w(),r(),q(),ctx.fillStyle="#000",ctx.fillRect(0,0,v.w,v.h)},ctx.fillStyle="#000",ctx.fillRect(0,0,v.w,v.h);
+
+var iterations=5000,
+    X = -0.7463,
+    Y =  0.1102,
+    R =  0.005,
+    xmin=X-R,xmax=X+R,
+    ymin=Y-R,ymax=Y+R;
+
+var mandelbrotIteration=function(cx,cy,maxiter){
+  var x=0.0,y=0.0,tmp=0;
+  for(var i=0;i<maxiter&&x*x+y*y<=4;++i){
+    tmp=2*x*y;
+    x=x*x-y*y+cx;
+    y=tmp+cy;
+  } //end for
+  return i;
 }; 
-var mainLoop=function(cur){
-	for(var ix=cur;ix<cur+1&&ix<v.w;++ix){
-		for(var iy=0;iy<v.h;++iy){
-			var x=xmin+(xmax-xmin)*ix/(v.w-1);
-			var y=ymin+(ymax-ymin)*iy/(v.h-1);
-			var i=Mandeliter(x,y,iterations);
-			if(i==iterations){
-				ctx.fillStyle="#000";
-			}else{
-				var c=3*Math.log(i)/Math.log(iterations-1.0);
-				if(c<1){
-					ctx.fillStyle="rgb(0,0,"+Math.floor(255*c)+")";
-				}else if(c<2){
-					ctx.fillStyle="rgb(0,"+Math.floor(255*c)+",255)";
-				}else{
-					ctx.fillStyle="rgb("+Math.floor(255*(c-2))+",255,255)";
-				} //end if
-			} //end if
-			ctx.fillRect(ix,iy,3,3);
-		} //end for
-	} //end for
-	if(ix<v.w)setTimeout("mainLoop("+ix+")",1);
-};
-mainLoop(0);
+(function mainLoop(cur){
+  var c,r,g,b, //variables used to distinguish color
+      ix,iy,x,y,i; //location and iteration location variables
+
+  for(var ix=cur;ix<cur+1&&ix<v.w;++ix){
+    for(var iy=0;iy<v.h;++iy){
+      x = xmin+(xmax-xmin)*ix/(v.w-1);
+      y = ymin+(ymax-ymin)*iy/(v.h-1);
+      i = mandelbrotIteration(x,y,iterations);
+      c = 0xffffff/3*3*Math.log(i)/Math.log(iterations-1.0);
+      r = (c & 0xff0000) >> 16;
+      g = (c & 0x00ff00) >> 8;
+      b = (c & 0x0000ff);
+      ctx.fillStyle='rgb('+r+','+g+','+b+')';
+      ctx.fillRect(ix,iy,3,3);
+    } //end for
+  } //end for
+  if(ix<v.w)setTimeout(function(){mainLoop(ix)},1);
+})(0);
