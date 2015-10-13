85bd87ead8fc51af060196351dade13844aeaeb7
Nathaniel Inman <nate041010@gmail.com>
2015-10-13 18:54:26 -0500

    converted tab to space characters in barnsleyfern.

--- complex_math/BarnsleyFern.js
+++ complex_math/BarnsleyFern.js
@@ -13,12 +13,12 @@ var f4=function(){pen={x:-0.15*pen.x+0.28*pen.y,y:0.26*pen.x+0.24*pen.y+0.44};};
 easel.onDraw = function draw(cur){
   cur=cur||0;
   ctx.fillStyle='#0F0';
-	for(var i=cur,k=0;i<totalPoints&&i<cur+1000;i++){
-		k=r(100);
-		if(k<=fp[0]){f1();}else if(k<=fp[1]){f2();}else if(k<=fp[2]){f3();}else{f4();}
-		ctx.fillRect(pen.x*v.w/10+v.w/4,pen.y*v.h/10,2,2);
-	} //end for
-	if(cur<totalPoints)setTimeout(function(){draw(++cur)},1);
+  for(var i=cur,k=0;i<totalPoints&&i<cur+1000;i++){
+    k=r(100);
+    if(k<=fp[0]){f1();}else if(k<=fp[1]){f2();}else if(k<=fp[2]){f3();}else{f4();}
+    ctx.fillRect(pen.x*v.w/10+v.w/4,pen.y*v.h/10,2,2);
+  } //end for
+  if(cur<totalPoints)setTimeout(function(){draw(++cur)},1);
 };
 easel.redraw();
 //mainLoop(0); //initialize
