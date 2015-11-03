8c43528c3bb8ec7cc77010ca499cf0deea36bc78
Nathaniel Inman <nate041010@gmail.com>
2015-11-02 21:35:58 -0600

    diamond square algorihtm will now reset on resize.

--- complex_math/DiamondSquareAlgorithm.js
+++ complex_math/DiamondSquareAlgorithm.js
@@ -1,41 +1,49 @@
-var mapWidth=70,mapHeight=50;
-var map=(function(){
-  var map=[],color;
+// Public Constants
+var mapWidth,mapHeight,map=[];
+
+// Variable Configuration
+easel.config = function(){
+  var color;
+
+  map=[];
+  mapWidth=70;
+  mapHeight=50;
   for(var i=0;i<mapHeight;i++){
     map[i]=[];
     for(var j=0;j<mapWidth;j++){      
-      color=random(235);
+      color=r(235,0,1);
       map[i][j]=color;
     } //end for
   } //end for
-  return map;
-})();
+} //end easel.config()
 
 easel.onDraw = function main(x,y){
-  var cornerDistance1,cornerDistance2,cornerDistance3,cornerDistance4,
-      cornerWeight1,cornerWeight2,cornerWeight3,cornerWeight4,
-    cornerColor1,cornerColor2,cornerColor3,cornerColor4,
-    cellWidth=Math.ceil(v.w/mapWidth),cellHeight=Math.ceil(v.h/mapHeight),
-    totalWeight,color;
+  var cd1,cd2,cd3,cd4, //corner distance
+      cw1,cw2,cw3,cw4, //corner weight
+      cc1,cc2,cc3,cc4, //corner color
+      cellWidth=Math.ceil(v.w/mapWidth),cellHeight=Math.ceil(v.h/mapHeight),
+      totalWeight,color;
+
+  x=x||0;y=y||0; //catch-all, and on inits
   for(var j=0;j<cellHeight;j++){
     for(var i=0;i<cellWidth;i++){
-      cornerDistance1=Math.sqrt(Math.pow(i,2)+Math.pow(j,2));
-      cornerDistance2=Math.sqrt(Math.pow(cellWidth-i,2)+Math.pow(j,2));
-      cornerDistance3=Math.sqrt(Math.pow(i,2)+Math.pow(cellHeight-j,2));
-      cornerDistance4=Math.sqrt(Math.pow(cellWidth-i,2)+Math.pow(cellHeight-j,2));
-      totalWeight=cornerDistance1+cornerDistance2+cornerDistance3+cornerDistance4;
-      cornerWeight4=cornerDistance1/totalWeight;
-      cornerWeight3=cornerDistance2/totalWeight;
-      cornerWeight2=cornerDistance3/totalWeight;
-      cornerWeight1=cornerDistance4/totalWeight;
-      cornerColor1=map[y][x];
-      cornerColor2=x<mapWidth-1?map[y][x+1]:0;
-      cornerColor3=y<mapHeight-1?map[y+1][x]:0;
-      cornerColor4=x<mapWidth-1&&y<mapHeight-1?map[y+1][x+1]:0;
-      color=Math.floor(cornerColor1*cornerWeight1+
-        cornerColor2*cornerWeight2+
-        cornerColor3*cornerWeight3+
-        cornerColor4*cornerWeight4);
+      cd1=Math.sqrt(Math.pow(i,2)+Math.pow(j,2));
+      cd2=Math.sqrt(Math.pow(cellWidth-i,2)+Math.pow(j,2));
+      cd3=Math.sqrt(Math.pow(i,2)+Math.pow(cellHeight-j,2));
+      cd4=Math.sqrt(Math.pow(cellWidth-i,2)+Math.pow(cellHeight-j,2));
+      totalWeight=cd1+cd2+cd3+cd4;
+      cw4=cd1/totalWeight;
+      cw3=cd2/totalWeight;
+      cw2=cd3/totalWeight;
+      cw1=cd4/totalWeight;
+      cc1=map[y][x];
+      cc2=x<mapWidth-1?map[y][x+1]:0;
+      cc3=y<mapHeight-1?map[y+1][x]:0;
+      cc4=x<mapWidth-1&&y<mapHeight-1?map[y+1][x+1]:0;
+      color=Math.floor(cc1*cw1+
+        cc2*cw2+
+        cc3*cw3+
+        cc4*cw4);
       ctx.fillStyle='rgb('+color+','+color+','+color+')';
       ctx.fillRect(v.w/mapWidth*x+i,v.h/mapHeight*y+j,1,1);
     } //end for
@@ -46,7 +54,5 @@ easel.onDraw = function main(x,y){
   }else if(y<mapHeight-1){
     easel.redraw(0,++y);
   } //end if
-  easel.redraw
 };
-easel.redraw(0,0);
-mainLoop(0,0);
+easel.redraw();
