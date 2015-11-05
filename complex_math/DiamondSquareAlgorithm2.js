77237c3fce7de2a4d804c8dbd4acf4dee4a7b71e
Nathaniel Inman <nate041010@gmail.com>
2015-11-05 08:16:31 -0600

    updated diamond square algorithm, cleaned up some code.

--- complex_math/DiamondSquareAlgorithm2.js
+++ complex_math/DiamondSquareAlgorithm2.js
@@ -1,5 +1,5 @@
 // Constants
-var map=[],SIZE=200;
+var map=[],s=200;
 var giveSpot=function(i,j){
   map[i-1][j-1]+=5;map[i  ][j-1]+=5;map[i+1][j-1]+=5;
   map[i  ][j  ]+=5;map[i  ][j  ]+=9;map[i+1][j  ]+=5;
@@ -7,26 +7,28 @@ var giveSpot=function(i,j){
 }; //end giveSpot()
 
 // Set up the main draw function
-easel.onDraw = function(){
-  for(var i=0;i<SIZE;i++){map[i]=[];for(var j=0;j<SIZE;j++){map[i][j]=0;}}
-  for(var i=0;i<SIZE*SIZE;i++){
-    giveSpot(1+r(SIZE-2,0,1),1+r(SIZE-2,0,1));
+easel.onDraw = function main(){
+  var i,j;
+
+  for(i=0;i<s;i++){map[i]=[];for(j=0;j<s;j++){map[i][j]=0;}}
+  for(i=0;i<s*s;i++){
+    giveSpot(1+r(s-2,0,1),1+r(s-2,0,1));
   } //end for
-  for(var i=0;i<SIZE;i++){for(var j=0;j<SIZE;j++){
+  for(i=0;i<s;i++){for(j=0;j<s;j++){
     var amt=0,num=0;
     if(i-1>=0){  amt+=map[i-1][j];num++;}
-    if(i+1<SIZE){amt+=map[i+1][j];num++;}
+    if(i+1<s){amt+=map[i+1][j];num++;}
     if(j-1>=0){  amt+=map[i][j-1];num++;}
-    if(j+1<SIZE){amt+=map[i][j+1];num++;}
+    if(j+1<s){amt+=map[i][j+1];num++;}
     if(i-1>=0  &&j-1>=0  ){amt+=map[i-1][j-1]/2;num+=0.5;}
-    if(i+1<SIZE&&j-1>=0  ){amt+=map[i+1][j-1]/2;num+=0.5;}
-    if(i-1>=0  &&j+1<SIZE){amt+=map[i-1][j+1]/2;num+=0.5;}
-    if(i+1<SIZE&&j+1<SIZE){amt+=map[i+1][j+1]/2;num+=0.5;}
+    if(i+1<s&&j-1>=0  ){amt+=map[i+1][j-1]/2;num+=0.5;}
+    if(i-1>=0  &&j+1<s){amt+=map[i-1][j+1]/2;num+=0.5;}
+    if(i+1<s&&j+1<s){amt+=map[i+1][j+1]/2;num+=0.5;}
     map[i][j]=Math.floor(amt/num);
   }} //end for*2
-  for(var i=0;i<SIZE;i++){for(var j=0;j<SIZE;j++){
+  for(i=0;i<s;i++){for(j=0;j<s;j++){
     ctx.fillStyle='rgb('+map[i][j]+','+map[i][j]+','+(map[i][j]*2)+')';
-    ctx.fillRect(i*v.w/SIZE,j*v.h/SIZE,v.w/SIZE+1,v.h/SIZE+1);
+    ctx.fillRect(i*v.w/s,j*v.h/s,v.w/s+1,v.h/s+1);
   }} //end for*2
 } //end easel.onDraw()
 easel.redraw();
