2cb20ac9fc14f39d12bb4d882d35ec72916264b9
Nathaniel Inman <nate041010@gmail.com>
2015-11-06 15:47:45 -0600

    dla goes up and down and looks pretty cool now.

--- complex_math/DiffusionLimitedAggregation.js
+++ complex_math/DiffusionLimitedAggregation.js
@@ -3,9 +3,8 @@ var map=(function(){
   for(var i=0;i<v.h;i++){
     map[i]=[];
     for(var j=0;j<v.w;j++){
-      if(i==v.h-1){
+      if(i>v.h-10){
         map[i][j]=true;
-        ctx.fillRect(j,i,1,1);
       }else{
         map[i][j]=false;
       }//end if
@@ -13,16 +12,29 @@ var map=(function(){
   } //end for
   return map;
 })();
+var d=1;
 easel.onDraw = function(highestY){
-  ctx.fillStyle='#FFF';
+  var worked = false;
+  highestY=highestY||0;
+  if(d){
+    ctx.fillStyle='rgba(255,0,0,0.2)';
+  }else{
+    ctx.fillStyle='rgba(0,0,0,0.2)';
+  } //end if
   var walker=function(x,y){
     var percent=r(100,0,1);
     if(x-1>=0&&percent<20){
       ctx.fillRect(x-1,y,1,1);
       map[y][x-1]=true;walker(x-1,y);
     }else if(y-1>=0&&percent<70){
-      ctx.fillRect(x,y-1,1,1);
-      map[y-1][x]=true;walker(x,y-1);
+      // going upwards or downwards
+      if(d){
+        ctx.fillRect(x,y-1,1,1);
+        map[y-1][x]=true;walker(x,y-1);
+      }else if(y+1<v.h){
+        ctx.fillRect(x,y+1,1,1);
+        map[y+1][x]=true;walker(x,y+1);
+      } //end if
     }else if(x+1<v.w&&percent<90){
       ctx.fillRect(x+1,y,1,1);
       map[y][x+1]=true;walker(x+1,y);
@@ -30,20 +42,26 @@ easel.onDraw = function(highestY){
       return y;
     } //end if    
   };
-  for(iter=0;iter<100;iter++){
-    var x=r(v.w,0,1),y=r(highestY>50?50:highestY,v.h-highestY-1,1),worked=false,n;
+  for(iter=0;iter<200;iter++){
+    var x=r(v.w,0,1),y=v.h-highestY-1,n;
     if(y+1<v.h&&map[y+1][x]==true||
        x+1<v.w&&map[y][x+1]==true||
-       x-1>=0&& map[y][x-1]==true)worked=true;
-    if(worked){
-      if(v.h-y>highestY)highestY=v.h-y;
+       x-1>=0&& map[y][x-1]==true){
+      worked=true;
       map[y][x]=true;
       ctx.fillRect(x,y,1,1);
-      n=walker(x,y)
-      if(v.h-n>highestY)highestY=v.h-n;
-      if(highestY==v.h)return;
-    }//end if
+      n=walker(x,y);
+    } //end ifworked=true;
   } //end for
+
+  // Only keep drawing if we aren't at the top
+  if(worked&&highestY<v.h-1&&d){
+    highestY++;
+    d=highestY<v.h-1?d:0;
+  }else{
+    highestY--;
+    d=highestY>0?0:1;
+  }//end if
   easel.redraw(highestY);
 };
 easel.redraw();
