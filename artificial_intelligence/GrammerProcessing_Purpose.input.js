a99417c9d27e57ae940123cce14450f02dcb5b0f
Nathaniel Inman <nate041010@gmail.com>
2015-12-01 08:36:09 -0600

    switching double quotes to single quotes.

--- artificial_intelligence/GrammerProcessing_Purpose.input.js
+++ artificial_intelligence/GrammerProcessing_Purpose.input.js
@@ -1,13 +1,14 @@
 function cmdKeyDown(){
   if(event.keyCode==13){
-    var message=document.getElementById("cmd").value;
-    document.getElementById("cmd").value="";
+    var message=document.getElementById('cmd').value;
+    document.getElementById('cmd').value='';
     core.process(message);
   } //end if
 } //end cmdKeyDown()
 function enterKeyDown(){
-  var message=document.getElementById("cmd").value;
-  document.getElementById("cmd").value=""; 
+  var message=document.getElementById('cmd').value;
+  document.getElementById('cmd').value=''; 
+  document.getElementById('output').innerHTML='';
   core.process(message);
 } //end enterKeyDown(){
 
