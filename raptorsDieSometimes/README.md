8dc572ae51a6f8554230d1301e778e77dba93a59
Nathaniel Inman <nate041010@gmail.com>
2017-01-08 17:16:42 -0600

    added raptors die sometimes experiment I did for fun a few years ago.

---
+++ raptorsDieSometimes/README.md
@@ -0,0 +1,4 @@
+raptorsDieSometimes
+===================
+
+[![Build Status](https://travis-ci.org/Nathaniel Inman/raptorsDieSometimes.svg?branch=master)](https://travis-ci.org/Nathaniel Inman/raptorsDieSometimes) [![dependency Status](https://david-dm.org/Nathaniel Inman/raptorsDieSometimes/status.svg?style=flat)](https://david-dm.org/Nathaniel Inman/raptorsDieSometimes) [![devDependency Status](https://david-dm.org/Nathaniel Inman/raptorsDieSometimes/dev-status.svg?style=flat)](https://david-dm.org/Nathaniel Inman/raptorsDieSometimes#info=devDependencies)
---
+++ raptorsDieSometimes/src/app/app.js
@@ -0,0 +1,21 @@
+import 'file-loader?name=[name].html!./index.jade';
+import './app.styl';
+import {easel} from 'ion-cloud';
+import {main} from './main';
+
+// Launch application if easel was able to create a canvas,
+// if it wasn't then we know canvas isn't supported
+let noscript = document.getElementById('noscript');
+
+if(!easel.activated){
+  noscript.innerHTML = `
+  <p class="browsehappy">
+    You are using an outdated browser. Please
+    <a href="http://browsehappy.com/"> upgrade your browser</a>
+    to improve your experience.
+    <span style="color:red;"><br/>Canvas isn't supported in your browser.</span>
+  </p>`;
+}else{
+  noscript.style.visibility='hidden';
+  main();
+} //end if
