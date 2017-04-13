398b3905ae1bcdc5471378597183d82ac2540913
Nathaniel Inman <nate041010@gmail.com>
2017-04-13 15:33:37 -0500

    simplified the formulas quite a bit so theyre easier to understand.

--- textureManipulation/src/app/main.js
+++ textureManipulation/src/app/main.js
@@ -31,21 +31,27 @@ export function main() {
     let blend1 = ctx.getImageData(w*2,h*2,w,h),
         blend2 = ctx.getImageData(w*2,h*2,w,h),
         blend3 = ctx.getImageData(w*2,h*2,w,h),
-        blend4 = ctx.getImageData(w*2,h*2,w,h);
+        blend4 = ctx.getImageData(w*2,h*2,w,h),
+        blendt = ctx.getImageData(w*2,h*2,w,h);
 
     console.log(img1.data.length);
-    for(let i=0,vperc,hperc;i<img1.data.length;i++){
-      vperc = i/img1.data.length;
-      hperc = ((i/4)%w)/w;
+    for(let i=0,x,y,vperc,hperc,rperc;i<img1.data.length;i++){
+      x = (i/4)%w;
+      y = Math.floor((i/4)/w);
+      vperc = y/h;
+      hperc = x/w; //f(x) = x / maxX
+      rperc = ((i/4)%w); //f(x) = 1 / 2x
       blend1.data[i] = img1.data[i]*(1-vperc)+img2.data[i]*vperc;
       blend2.data[i] = img1.data[i]*vperc+img2.data[i]*(1-vperc);
       blend3.data[i] = img1.data[i]*(1-hperc)+img2.data[i]*hperc;
       blend4.data[i] = img1.data[i]*hperc+img2.data[i]*(1-hperc);
+      blendt.data[i] = img1.data[i]*(1-rperc)+img2.data[i]*rperc;
     } //end for
     ctx.putImageData(blend2,w*2,h);
     ctx.putImageData(blend1,w*2,h*3);
     ctx.putImageData(blend4,w,h*2);
     ctx.putImageData(blend3,w*3,h*2);
+    ctx.putImageData(blendt,w,h);
     //requestAnimationFrame(loop);
   } //end draw();
 } //end app()
