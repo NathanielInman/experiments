a5f58cbd96deb767fc22ffa428e7ef4391878765
Nathaniel Inman <nate041010@gmail.com>
2017-04-13 16:21:48 -0500

    finished all blurs in a clean way.

--- textureManipulation/src/app/main.js
+++ textureManipulation/src/app/main.js
@@ -49,17 +49,19 @@ export function main() {
         blend4 = ctx.getImageData(w*2,h*2,w,h),
         blend5 = ctx.getImageData(w*2,h*2,w,h),
         blend6 = ctx.getImageData(w*2,h*2,w,h),
-        blend7 = ctx.getImageData(w*2,h*2,w,h);
+        blend7 = ctx.getImageData(w*2,h*2,w,h),
+        blend8 = ctx.getImageData(w*2,h*2,w,h);
 
     console.log(img1.data.length);
-    for(let i=0,x,y,vperc,hperc,lrperc,tlperc,trperc;i<img1.data.length;i++){
-      x = (i/4)%w;
+    for(let i=0,x,y,vperc,hperc,lrperc,tlperc,trperc,llperc;i<img1.data.length;i++){
+      x = (i/4)%w; //image data contains 4 entries per pixel: r,g,b,a
       y = Math.floor((i/4)/w);
       vperc = y/h;
       hperc = x/w;
       lrperc = (x+y)/((w+h)*.5);
       tlperc = (x+y)/((w+h)*.5)-1;
       trperc = (x-y)/h+1;
+      llperc = (y-x)/w+1;
       blend1.data[i] = img1.data[i]*(1-vperc)+img2.data[i]*vperc;
       blend2.data[i] = img1.data[i]*vperc+img2.data[i]*(1-vperc);
       blend3.data[i] = img1.data[i]*(1-hperc)+img2.data[i]*hperc;
@@ -67,6 +69,7 @@ export function main() {
       blend5.data[i] = img1.data[i]*(1-lrperc)+img2.data[i]*lrperc;
       blend6.data[i] = img1.data[i]*tlperc+img2.data[i]*(1-tlperc);
       blend7.data[i] = img1.data[i]*(1-trperc)+img2.data[i]*trperc;
+      blend8.data[i] = img1.data[i]*(1-llperc)+img2.data[i]*llperc;
     } //end for
     ctx.putImageData(blend2,w*2,h);
     ctx.putImageData(blend1,w*2,h*3);
@@ -75,6 +78,7 @@ export function main() {
     ctx.putImageData(blend5,w*3,h*3);
     ctx.putImageData(blend6,w,h);
     ctx.putImageData(blend7,w*3,h);
+    ctx.putImageData(blend8,w,h*3);
     //requestAnimationFrame(loop);
   } //end draw();
 } //end app()
