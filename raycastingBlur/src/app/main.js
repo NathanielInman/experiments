184a66f33eda15f07808d7c2ca449b5ba9f6177e
Nathaniel Inman <nate041010@gmail.com>
2017-04-14 10:11:24 -0500

    breaking state of raytracing.

--- raycastingBlur/src/app/main.js
+++ raycastingBlur/src/app/main.js
@@ -2,85 +2,26 @@ import {easel} from './app';
 
 // Execute the main function, blurring the textures
 export function main() {
-  let t1 = new Image(),
-      t2 = new Image();
+  let img1,img2,blend,
+    w = Math.floor(v.w/5), h = Math.floor(v.h/5),
+    r = w; //blur radius
 
-  // Lady shirt in the background
-  t1.src = 'http://i.imgur.com/K4sPN88.jpg';
-  t1.crossOrigin = 'Anonymous';
-  t2.src = 'http://i.imgur.com/HZdyZam.jpg';
-  t2.crossOrigin = 'Anonymous';
-  setTimeout(loop,3000);
+  ctx.fillStyle = '#f00';
+  ctx.fillRect(w,h,w*3,h*3);
+  img1 = ctx.getImageData(w,h,w*3,h*3);
+  ctx.fillStyle = '#00f';
+  ctx.fillRect(w,h,w*3,h*3);
+  img2 = ctx.getImageData(w,h,w*3,h*3);
+  blend = ctx.getImageData(w,h,w*3,h*3);
 
-  function loop(){
-    let img1,img2,w = Math.floor(v.w/5), h = Math.floor(v.h/5);
-
-    /*
-    ctx.drawImage(t1,w,h,w,h);
-    ctx.drawImage(t1,w*2,h,w,h);
-    ctx.drawImage(t1,w*3,h,w,h);
-    ctx.drawImage(t1,w,h*2,w,h);
-    ctx.drawImage(t1,w*3,h*2,w,h);
-    ctx.drawImage(t1,w,h*3,w,h);
-    ctx.drawImage(t1,w*2,h*3,w,h);
-    ctx.drawImage(t1,w*3,h*3,w,h);
-    ctx.drawImage(t2,w*2,h*2,w,h);
-    img1 = ctx.getImageData(w*2,h*2,w,h);
-    img2 = ctx.getImageData(w,h,w,h);
-    */
-    ctx.fillStyle = '#f00';
-    ctx.fillRect(w,h,w,h);
-    ctx.fillRect(w*2,h,w,h);
-    ctx.fillRect(w*3,h,w,h);
-    ctx.fillRect(w,h*2,w,h);
-    ctx.fillRect(w*3,h*2,w,h);
-    ctx.fillRect(w,h*3,w,h);
-    ctx.fillRect(w*2,h*3,w,h);
-    ctx.fillRect(w*3,h*3,w,h);
-    ctx.fillStyle = '#00f';
-    ctx.fillRect(w*2,h*2,w,h);
-    img1 = ctx.getImageData(w*2,h*2,w,h);
-    img2 = ctx.getImageData(w,h,w,h);
-
-    // now draw blends
-    let blend1 = ctx.getImageData(w*2,h*2,w,h),
-        blend2 = ctx.getImageData(w*2,h*2,w,h),
-        blend3 = ctx.getImageData(w*2,h*2,w,h),
-        blend4 = ctx.getImageData(w*2,h*2,w,h),
-        blend5 = ctx.getImageData(w*2,h*2,w,h),
-        blend6 = ctx.getImageData(w*2,h*2,w,h),
-        blend7 = ctx.getImageData(w*2,h*2,w,h),
-        blend8 = ctx.getImageData(w*2,h*2,w,h);
-
-    console.log(img1.data.length);
-    for(let i=0,x,y,vperc,hperc,lrperc,tlperc,trperc,llperc;i<img1.data.length;i++){
-      if(i%4===0){ //limit calculations to 1/4th
-        x = Math.floor(i/4)%w; //image data contains 4 entries per pixel: r,g,b,a
-        y = Math.floor(Math.floor(i/4)/w);
-        vperc = y/h;
-        hperc = x/w;
-        lrperc = (x+y)/((w+h)*.5);
-        tlperc = (x+y)/((w+h)*.5)-1;
-        trperc = (x-y)/h+1;
-        llperc = (y-x)/w+1;
-      } //end if
-      blend1.data[i] = img1.data[i]*(1-vperc)+img2.data[i]*vperc;
-      blend2.data[i] = img1.data[i]*vperc+img2.data[i]*(1-vperc);
-      blend3.data[i] = img1.data[i]*(1-hperc)+img2.data[i]*hperc;
-      blend4.data[i] = img1.data[i]*hperc+img2.data[i]*(1-hperc);
-      blend5.data[i] = img1.data[i]*(1-lrperc)+img2.data[i]*lrperc;
-      blend6.data[i] = img1.data[i]*tlperc+img2.data[i]*(1-tlperc);
-      blend7.data[i] = img1.data[i]*(1-trperc)+img2.data[i]*trperc;
-      blend8.data[i] = img1.data[i]*(1-llperc)+img2.data[i]*llperc;
-    } //end for
-    ctx.putImageData(blend2,w*2,h);
-    ctx.putImageData(blend1,w*2,h*3);
-    ctx.putImageData(blend4,w,h*2);
-    ctx.putImageData(blend3,w*3,h*2);
-    ctx.putImageData(blend5,w*3,h*3);
-    ctx.putImageData(blend6,w,h);
-    ctx.putImageData(blend7,w*3,h);
-    ctx.putImageData(blend8,w,h*3);
-    //requestAnimationFrame(loop);
-  } //end draw();
-} //end app()
+  for(let i=0,x,y,bk,ox=w*2+w/2,oy=h*2+h/2;i<img1.data.length;i++){
+    if(i%4===0){ //limit needless calculations to per pixel distance
+      x = Math.floor(i/4)%w; //image data contains 4 entries per pixel: r,g,b,a
+      y = Math.floor(Math.floor(i/4)/w);
+      bk = Math.sqrt(Math.pow(ox-x-w,2)+Math.pow(oy-y-h,2))/r;
+    } //end if
+    blend.data[i] = img1.data[i]*(1-bk)+img2.data[i]*bk;
+  } //end for
+  ctx.putImageData(blend,w,h);
+  //requestAnimationFrame(loop);
+} //end main()
