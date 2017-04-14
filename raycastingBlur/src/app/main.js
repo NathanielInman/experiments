3b1725582cf001a79091fe5f1824b53720aaff99
Nathaniel Inman <nate041010@gmail.com>
2017-04-14 10:29:11 -0500

    everything is incredibly more simple with just the addition of a couple variables.

--- raycastingBlur/src/app/main.js
+++ raycastingBlur/src/app/main.js
@@ -3,26 +3,28 @@ import {easel} from './app';
 // Execute the main function, blurring the textures
 export function main() {
   let img1,img2,blend,
-    w = Math.floor(v.w/5), h = Math.floor(v.h/5),
-    r = w*2; //blur radius
+      sx = Math.floor(v.w/5),
+      sy = Math.floor(v.h/5),
+      w = Math.floor(v.w/5*3),
+      h = Math.floor(v.h/5*3),
+      r = w/2; //blur radius
 
   ctx.fillStyle = '#f00';
-  ctx.fillRect(w,h,w*3,h*3);
-  img1 = ctx.getImageData(w,h,w*3,h*3);
+  ctx.fillRect(sx,sy,w,h);
+  img1 = ctx.getImageData(sx,sy,w,h);
   ctx.fillStyle = '#00f';
-  ctx.fillRect(w,h,w*3,h*3);
-  img2 = ctx.getImageData(w,h,w*3,h*3);
-  blend = ctx.getImageData(w,h,w*3,h*3);
+  ctx.fillRect(sx,sy,w,h);
+  img2 = ctx.getImageData(sx,sy,w,h);
+  blend = ctx.getImageData(sx,sy,w,h);
 
-  console.log(img1.data.length,img2.data.length,blend.data.length);
-  for(let i=0,x,y,bk,ox=w*2+w/2,oy=h*2+h/2;i<img1.data.length;i++){
+  for(let i=0,x,y,bk,ox=sx+w/2,oy=sy+h/2;i<blend.data.length;i++){
     if(i%4===0){ //limit needless calculations to per pixel distance
-      x = Math.floor(i/4)%(w*3); //image data contains 4 entries per pixel: r,g,b,a
-      y = Math.floor(Math.floor(i/4)/(w*3));
-      bk = Math.sqrt(Math.pow(ox-x-w,2)+Math.pow(oy-y-h,2))/r;
+      x = Math.floor(i/4)%w; //image data contains 4 entries per pixel: r,g,b,a
+      y = Math.floor(Math.floor(i/4)/w);
+      bk = Math.sqrt(Math.pow(ox-x-sx,2)+Math.pow(oy-y-sy,2))/r;
     } //end if
     blend.data[i] = img1.data[i]*(1-bk)+img2.data[i]*bk;
   } //end for
-  ctx.putImageData(blend,w,h);
+  ctx.putImageData(blend,sx,sy);
   //requestAnimationFrame(loop);
 } //end main()
