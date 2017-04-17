61653e2fd01cf5d8d0026dd4b36dee3592d1d4e1
Nathaniel Inman <nate041010@gmail.com>
2017-04-17 09:54:39 -0500

    tile blur kinda works but it doesnt skew or sketch.

--- textureManipulation/raycastingTileBlur/src/app/main.js
+++ textureManipulation/raycastingTileBlur/src/app/main.js
@@ -5,17 +5,17 @@ export function main() {
   let img1,img2,blend,t1,t2,
       sx = Math.floor(v.w/5),
       sy = Math.floor(v.h/5),
-      w = Math.floor(v.w/5*3),
-      h = Math.floor(v.h/5*3),
+      w = Math.floor(v.w/5),
+      h = Math.floor(v.h/5),
       s = w/4, //spread radius
       r = w/2; //blur radius
 
   ctx.fillStyle = '#f00';
-  ctx.fillRect(sx,sy,w,h);
+  ctx.fillRect(sx,sy,w*3,h*3);
   img1 = ctx.getImageData(sx,sy,w,h);
   ctx.fillStyle = '#00f';
-  ctx.fillRect(sx,sy,w,h);
-  img2 = ctx.getImageData(sx,sy,w,h);
+  ctx.fillRect(sx*2,sy*2,w,h);
+  img2 = ctx.getImageData(sx*2,sy*2,w,h);
   blend = ctx.getImageData(sx,sy,w,h);
   t1 = performance.now();
   for(let i=0,x,y,bk,ox=sx+w/2,oy=sy+h/2;i<blend.data.length;i++){
@@ -24,9 +24,9 @@ export function main() {
       y = Math.floor(Math.floor(i/4)/w);
       bk = (Math.sqrt(Math.pow(ox-x-sx,2)+Math.pow(oy-y-sy,2))-s)/(r-s);
     } //end if
-    blend.data[i] = img1.data[i]*(1-bk)+img2.data[i]*bk;
+    blend.data[i] = img1.data[i]*bk+img2.data[i]*(1-bk);
   } //end for
   t2 = performance.now();
-  ctx.putImageData(blend,sx,sy);
+  ctx.putImageData(blend,sx*2,sy*2);
   console.info(`Image processed in ${t2-t1} ms`);
 } //end main()
