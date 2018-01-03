520938df6683881c4d5b760b8f7b78cb0f2d88dd
Nathaniel Inman <nate@theoestudio.com>
2018-01-03 13:58:37 -0600

    updated spatial mapper to use webpack and pass linting checks.

--- objectCollision/spatialMapper/src/app/SpatialMapper.js
+++ objectCollision/spatialMapper/src/app/SpatialMapper.js
@@ -1,15 +1,20 @@
+const rd0 = uint=> Math.random()*uint;
+const rd = (lint,uint)=> Math.random()*(uint-lint)+lint;
+const ri0 = uint=> Math.floor(Math.random()*uint);
+
 class Entity{
-  constructor(x,y,id){
+  constructor(id,viewport){
     this.id = id; //The main identifier of the entity (unchanging);
+    this.viewport = viewport;
     this.iid = 0; //the location identifier (changes);
-    this.x = x||0;
-    this.y = y||0;
+    this.x = rd0(this.viewport.w);
+    this.y = rd0(this.viewport.h);
     this.w = 10;
     this.h = 10;
-    this.dx = r(2,0,1); //direction of x axis movement
-    this.dy = r(2,0,1); //direction of y axis movement
-    this.sx = r(1,5); //speed of x value
-    this.sy = r(1,5); //speed of y value
+    this.dx = ri0(2); //direction of x axis movement
+    this.dy = ri0(2); //direction of y axis movement
+    this.sx = rd(0.5,1.5); //speed of x value
+    this.sy = rd(0.5,1.5); //speed of y value
   }
 
   // Each entity blindly moves and knows not of other entities around
@@ -18,18 +23,24 @@ class Entity{
   move(){
     this.x=this.dx?this.x+=this.sx:this.x-=this.sx;
     this.y=this.dy?this.y+=this.sy:this.y-=this.sy;
-    if(this.x>v.w*0.99&&this.dx||this.x<v.w*0.01&&!this.dx)this.dx^=1;
-    if(this.y>v.h*0.99&&this.dy||this.y<v.h*0.01&&!this.dy)this.dy^=1;
+    if(this.x>this.viewport.w*0.99&&this.dx||this.x<this.viewport.w*0.01&&!this.dx)this.dx^=1;
+    if(this.y>this.viewport.h*0.99&&this.dy||this.y<this.viewport.h*0.01&&!this.dy)this.dy^=1;
   }
 }
-class SpatialMapper{
-  constructor(){
+export class SpatialMapper{
+  constructor(viewport,ctx){
+    this.viewport = viewport;
+    this.ctx = ctx;
     this.total = 500; //total number of entities we'll draw
     this.collection = []; //holds all of the entities
     this.mapperSize = 100;
-    this.mapper = (function(s){
-      var m = [],i,j; //shorten variables for simplicity
-      for(i=0;i<s;i++){m[i]=[];for(j=0;j<s;j++)m[i][j]=[];}
+    this.mapper = (s=>{
+      let m = [],i,j; //shorten variables for simplicity
+
+      for(i=0;i<s;i++){
+        m[i]=[];
+        for(j=0;j<s;j++) m[i][j]=[];
+      } //end for
       return m; //return the structured array mapperSize^2 length
     })(this.mapperSize);
     this.start();
@@ -42,7 +53,7 @@ class SpatialMapper{
   // in the cell and use that number as the index identification
   start(){
     for(let n=0;n<this.total;n++){
-      let e = new Entity(r(v.w),r(v.h),n),
+      let e = new Entity(n,this.viewport),
           i = this.getMapperIndex(e);
 
       i.push(e.id);
@@ -57,11 +68,11 @@ class SpatialMapper{
   // compute their movement, update their spatial index identifier.
   // We also draw each entity before their computation occurs
   loop(){
-    ctx.fillStyle='#000';
-    ctx.fillRect(0,0,v.w,v.h);
-    ctx.fillStyle='#F00';
+    this.ctx.fillStyle='#000';
+    this.ctx.fillRect(0,0,this.viewport.w,this.viewport.h);
+    this.ctx.fillStyle='#F00';
     this.collection.forEach((e)=>{
-      ctx.fillRect(e.x,e.y,e.w,e.h);
+      this.ctx.fillRect(e.x,e.y,e.w,e.h);
       this.checkBounce(e);
       e.move();
 
@@ -72,7 +83,7 @@ class SpatialMapper{
       e.iid = this.getMapperIndex(e).length;
       this.getMapperIndex(e).push(e.id);
     });
-    setTimeout(()=>this.loop(),16);
+    requestAnimationFrame(()=>this.loop());
   }
 
   // Acquire the spatial cell where this entity resides and return
@@ -86,8 +97,8 @@ class SpatialMapper{
   // to also make sure we don't exceed the constraints of the
   // array.
   getXIndex(e){
-    let s = this.mapperSize;
-    let i = M.floor(e.x/v.w*s);
+    let s = this.mapperSize, i = Math.floor(e.x/this.viewport.w*s);
+
     return i<0?0:i>s-1?s-1:i;
   }
 
@@ -95,16 +106,15 @@ class SpatialMapper{
   // viewport height wiht the height of the mapper. It's important
   // to also make sure we don't exceed the constraints of the array
   getYIndex(e){
-    let s = this.mapperSize;
-    let i = M.floor(e.y/v.h*s);
+    let s = this.mapperSize, i = Math.floor(e.y/this.viewport.h*s);
+
     return i<0?0:i>s-1?s-1:i;
   }
 
   // Check the current entities spatial cell to see if other
   // entities are there and thereby compute their collisions
   checkBounce(e){
-    var i=0,c=e,t,gi = this.getMapperIndex, //shorten names
-        sameCell=this.getMapperIndex(e);
+    var i=0,c=e,t, sameCell=this.getMapperIndex(e);
 
     // Iterate through the nearby and occupied collections
     // and check for other entities. This will warrant a collision
@@ -112,14 +122,14 @@ class SpatialMapper{
       t=this.collection[sameCell[i]];
 
       // Make sure target entity isn't current entity also
-      if(i!==t.id&&M.abs(c.x-t.x)/10<1&&M.abs(c.y-t.y)/10<1){
+      if(i!==t.id&&Math.abs(c.x-t.x)/10<1&&Math.abs(c.y-t.y)/10<1){
         // We have a collision, detect which axis will be
         // repelled so we have a directional movement
-        if(t.dx!=c.dx==1&&t.dy!=c.dy){
+        if(t.dx!==c.dx===1&&t.dy!==c.dy){
           c.dx^=1;t.dx^=1;c.dy^=1; //oblique collision
-        }else if(t.dx==c.dx&&t.dy!=c.dy){
+        }else if(t.dx===c.dx&&t.dy!==c.dy){
           c.dy^=1;t.dy^=1; //vertical collision
-        }else if(t.dx!=c.dx&&t.dy==c.dy){
+        }else if(t.dx!==c.dx&&t.dy===c.dy){
           c.dx^=1;t.dx^=1; //horizontal collision
         } //end if
         break; //prevents bouncing on two objects by leaving early
@@ -127,12 +137,3 @@ class SpatialMapper{
     } //end for
   }
 }
-
-/**
- * Main entry point to application
- */
-function app() {
-  "use strict";
-
-  var scene = new SpatialMapper();
-} //end app()
