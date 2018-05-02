5dc480e06ffea721e507ad0cc485492894f41938
Nathaniel Inman <nate@theoestudio.com>
2018-05-02 12:10:11 -0500

    created a biome experiment based on the erosion experiment.

---
+++ mapGeneration/biomes/README.md
@@ -0,0 +1,4 @@
+mapErosion
+===================
+
+[![Build Status](https://travis-ci.org/Nathaniel Inman/mapErosion.svg?branch=master)](https://travis-ci.org/Nathaniel Inman/mapErosion) [![dependency Status](https://david-dm.org/Nathaniel Inman/mapErosion/status.svg?style=flat)](https://david-dm.org/Nathaniel Inman/mapErosion) [![devDependency Status](https://david-dm.org/Nathaniel Inman/mapErosion/dev-status.svg?style=flat)](https://david-dm.org/Nathaniel Inman/mapErosion#info=devDependencies)
---
+++ mapGeneration/biomes/src/app/applyErosion.js
@@ -0,0 +1,55 @@
+export function applyErosion(map){
+  let n=null, //will hold lowest neighbor
+      mapHeight = map.length,
+      mapWidth = map[0].length,types={};
+
+  for(let raindrop=0,x,y,v;raindrop<40000;raindrop++){
+    x = 5+Math.floor(Math.random()*(mapWidth-10));
+    y = 5+Math.floor(Math.random()*(mapHeight-10));
+    n = getLowestNeighbor(map,x,y);
+    v = 0.005;
+    while(n){
+      map[y][x].height-=0.005;
+      types[n.type]=types[n.type]?types[n.type]+1:1;
+      x = n.x;
+      y = n.y;
+      map[y][x].height+=0.005;
+      v -= 0.0005; //reduce velocity and size displacement
+      n = v>0.002?getLowestNeighbor(map,x,y):null;
+    }
+  }
+} //end applyErosion()
+
+//eslint-disable-next-line complexity
+function getLowestNeighbor(map,x,y){
+  let h = map[y][x].height,
+      mapHeight = map.length,
+      mapWidth = map[0].length,
+      result = [];
+
+  if(x>0&&map[y][x-1].height<h){
+    result.push({x: x-1,y,v: map[y][x-1].height,type: 'west'});
+  } //end if
+  if(x<mapWidth-1&&map[y][x+1].height<h){
+    result.push({x: x+1,y,v: map[y][x+1].height,type: 'east'});
+  } //end if
+  if(y>0&&map[y-1][x].height<h){
+    result.push({x,y: y-1,v: map[y-1][x].height,type: 'north'});
+  } //end if
+  if(y<mapHeight-1&&map[y+1][x].height<h){
+    result.push({x,y: y+1,v: map[y+1][x].height,type: 'south'});
+  } //end if
+  if(x>0&&y>0&&map[y-1][x-1].height<h){
+    result.push({x: x-1,y: y-1,v: map[y-1][x-1].height,type: 'northwest'});
+  } //end if
+  if(x<mapWidth-1&&y>0&&map[y-1][x+1].height<h){
+    result.push({x: x+1,y: y-1,v: map[y-1][x+1].height,type: 'northeast'});
+  } //end if
+  if(x<mapWidth-1&&y<mapHeight-1&&map[y+1][x+1].height<h){
+    result.push({x: x+1,y: y+1,v: map[y+1][x+1].height,type: 'southeast'});
+  } //end if
+  if(x>0&&y<mapHeight-1&&map[y+1][x-1].height<h){
+    result.push({x: x-1,y: y+1,v: map[y+1][x-1].height,type: 'southwest'});
+  } //end if
+  return result.length?result.reduce((p,v)=> v.v<p.v?v:p):null;
+} //end getLowestNeighbor()
