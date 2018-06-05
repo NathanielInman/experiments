771a008bed4c5bb952fcf1dfe962509f9451b208
Nathaniel Inman <nate@theoestudio.com>
2018-06-05 12:45:01 -0500

    now using the updated library for Ion animation and fixed all linting issues.

--- textureManipulation/mapCanvasDynamicColors/src/app/app.js
+++ textureManipulation/mapCanvasDynamicColors/src/app/app.js
@@ -1,21 +1,12 @@
 import {ink,Easel} from 'ion-cloud';
-import {Ion} from './Ion';
+import {Ion} from 'ion-cloud/src/lib/ion';
 import {Map} from './Map';
 import {environments} from './environments';
 import {Player} from './Player';
 
-window.ink = ink;
-
-let magic=new Ion(1000),
-    r=function(f,g,e){
-      g=g||0;e=e||true;
-      g=Math.random()*(g-f);
-      g=g<0?Math.random()*g:g+f;
-      return e==false?Math.floor(g):g
-    };
-
 let noscript = document.querySelector('noscript'),
     easel = new Easel(),
+    bubbles = new Ion(easel),
     environmentIndex = Math.floor(Math.random()*environments.length),
     environment = environments[environmentIndex],
     map = new Map(50,50,environment),
@@ -129,37 +120,37 @@ if(!easel.activated){
     easel.ctx.shadowColor = '#000';
     easel.ctx.shadowBlur = 15;
     easel.ctx.fill();
-    magic.background = easel.ctx.getImageData(0,0,easel.viewport.w,easel.viewport.h);
+    bubbles.background = easel.ctx.getImageData(0,0,easel.viewport.w,easel.viewport.h);
   };
   easel.redraw();
 
-  magic.easel          = easel;
-  magic.tween_duration = 1000;
-  magic.sx             = function(){return r(0,easel.viewport.w);};
-  magic.sy             = function(){return r(0,easel.viewport.h);};
-  magic.dx             = function(x){return x;};
-  magic.dy             = function(y){return y-50;};
-  magic.size           = function(){return r(0,2);};
-  magic.color          = function(atom){
-    let p, halfTween = magic.tween_duration/2;
+  bubbles.quantity = 1000;
+  bubbles.tweenDuration = 1000;
+  bubbles.startX = ()=> Math.random()*easel.viewport.w;
+  bubbles.startY = ()=> Math.random()*easel.viewport.h;
+  bubbles.endX = x=> x;
+  bubbles.endY = y=> y-50;
+  bubbles.size = ()=> Math.random()*2;
+  bubbles.color = (atom)=>{
+    let p, halfTween = bubbles.tweenDuration/2;
 
-    if(atom.c<=halfTween){
-      p = atom.c/halfTween;
+    if(atom.tweenCurrent<=halfTween){
+      p = atom.tweenCurrent/halfTween;
     }else{
-      p = (halfTween-Math.abs(halfTween-atom.c))/halfTween;
+      p = (halfTween-Math.abs(halfTween-atom.tweenCurrent))/halfTween;
     } //end if
     return `rgba(25,150,255,${p})`;
   };
-  magic.tween_type     = 6;
-  magic.onEscape       = function(atom){this.onEnd(atom);};
-  magic.onEnd          = function(atom){
-    this.particle[atom].x = this.particle[atom].sx = magic.sx();
-    this.particle[atom].y = this.particle[atom].sy = magic.sy();
-    this.particle[atom].dx = magic.dx(this.particle[atom].x);
-    this.particle[atom].dy = magic.dy(this.particle[atom].y);
-    this.particle[atom].c  =0;
+  bubbles.tweenType = 6;
+  bubbles.onEscape = function(atom){ this.onParticleEnd(atom); };
+  bubbles.onParticleEnd = function(atom){
+    atom.x = atom.startX = atom.originX = bubbles.startX();
+    atom.y = atom.startY = atom.originY = bubbles.startY();
+    atom.endX = atom.terminalX = bubbles.endX(atom.x);
+    atom.endY = atom.terminalY = bubbles.endY(atom.y);
+    atom.tweenCurrent = 0;
   };
-  magic.background = easel.ctx.getImageData(0,0,easel.viewport.w,easel.viewport.h);
-  magic.populate();
-  magic.process();
+  bubbles.background = easel.ctx.getImageData(0,0,easel.viewport.w,easel.viewport.h);
+  bubbles.populate();
+  bubbles.process();
 } //end if
