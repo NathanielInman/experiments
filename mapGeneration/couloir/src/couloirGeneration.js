fe3a32c96f9f18b74ec0de524fbce19d3192dbe1
Nathaniel Inman <nate@theoestudio.com>
2018-07-23 11:11:55 -0500

    now pruning non-walkable parts on couloir generator.

--- mapGeneration/couloir/src/couloirGeneration.js
+++ mapGeneration/couloir/src/couloirGeneration.js
@@ -12,4 +12,70 @@ export function couloir(map){
       } //end if
     });
   });
+  clipOrphaned(map);
 } //end function
+
+// Traverse a location completely
+function traverse(map,locStats,unmapped,x,y){
+  let newLoc = null; //we pull from unmapped
+
+  locStats.val=1; //set the current mas size to 1
+  map.setRoom({x,y,id: locStats.cur});
+  traverseLook(map,unmapped,x,y);
+  while(unmapped.length>0){
+    newLoc=unmapped.pop();
+    traverseLook(map,unmapped,newLoc.x,newLoc.y);
+    map.setRoom({x: newLoc.x,y: newLoc.y,id: locStats.cur});
+    locStats.val++;
+    if(locStats.val>locStats.max){
+      locStats.max=locStats.val;
+      locStats.num=locStats.cur;
+    } //end manage maximum mass
+  } //end while
+} //end traverse()
+
+//look around at location and push unmapped nodes to stack
+function traverseLook(map,unmapped,x,y){
+  if(x>0&&map.isWalkableOrEmpty({x: x-1,y})&&!map.getRoom({x: x-1,y})){
+    unmapped.push({x: x-1, y});
+    map.setRoom({x: x-1,y,id: -1});
+  } //end if
+  if(y>0&&map.isWalkableOrEmpty({x,y: y-1})&&!map.getRoom({x,y: y-1})){
+    unmapped.push({x,y: y-1});
+    map.setRoom({x,y: y-1,id: -1});
+  } //end if
+  if(x<map.width&&map.isWalkableOrEmpty({x: x+1,y})&&!map.getRoom({x: x+1,y})){
+    unmapped.push({x: x+1, y});
+    map.setRoom({x: x+1,y,id: -1});
+  } //end if
+  if(y<map.height&&map.isWalkableOrEmpty({x,y: y+1})&&!map.getRoom({x,y: y+1})){
+    unmapped.push({x,y: y+1});
+    map.setRoom({x,y: y+1,id: -1});
+  } //end if
+} //end traverseLook()
+
+// Remove orphaned floors by iterating through all sectors
+// and each time we find a floor we traverse from that section.
+// the largest section traversed is what we keep
+function clipOrphaned(map){
+  let locStats = {val: 0,cur: 0,num: 0,max: 0},
+      unmapped = [];
+
+  map.sectors.forEach((row,y)=>{
+    row.forEach((sector,x)=>{
+      if(sector.isWalkableOrEmpty()&&!sector.roomNumber){
+        locStats.cur++;
+        traverse(map,locStats,unmapped,x,y);
+      } //end if
+    });
+  });
+  map.sectors.forEach(row=>{
+    row.forEach(sector=>{
+      if(sector.isWalkableOrEmpty()&&sector.roomNumber!==locStats.num){
+        sector.setObstruction();
+      }else if(sector.isWalkableOrEmpty()&&!sector.isWater()){
+        sector.setFloor();
+      } //end if
+    });
+  });
+} //end clipOrphaned()
