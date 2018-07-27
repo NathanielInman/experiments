293878712c081a52e798418ee6c463c5ed2160c1
Nathaniel Inman <nate@theoestudio.com>
2018-07-27 10:57:42 -0500

    boilerplate for couloir generator ready.

--- mapGeneration/couloir/src/couloirGeneration.js
+++ mapGeneration/couloir/src/couloirGeneration.js
@@ -1,2 +1,16 @@
+import {Noise} from 'noisejs';
+
+const noise = new Noise(Math.random());
+
+console.log('noise',noise);
 export function couloir(map){
+  map.sectors.forEach(row=>{
+    row.forEach(sector=>{
+      if(noise.simplex2(sector.x/map.width*5,sector.y/map.height*5)<0.05){
+        sector.setFloor();
+      }else{
+        sector.setWall();
+      } //end if
+    });
+  });
 } //end function
