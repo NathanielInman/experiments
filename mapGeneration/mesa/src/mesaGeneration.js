d6c78738c842707223b97ef7065bcd30ca89847c
Nathaniel Inman <nate@theoestudio.com>
2018-07-30 11:13:38 -0500

    added new map generator: mesa

---
+++ mapGeneration/mesa/src/mesaGeneration.js
@@ -0,0 +1,89 @@
+import {Noise} from 'noisejs';
+
+const noise = new Noise(Math.random());
+
+export function mesa(map){
+  map.sectors.forEach(row=>{
+    row.forEach(sector=>{
+      let n = noise.simplex2(sector.x/map.width*5,sector.y/map.height*5);
+
+      if(n<0.05&&Math.random()<0.95){
+        sector.setFloor();
+      }else if(n<0.05){
+        sector.setObstruction();
+      }else if(n<0.2||Math.random()<0.2){
+        sector.setObstruction();
+      }else{
+        sector.setWall();
+      } //end if
+    });
+  });
+
+  // clip all non-walkable parts of the map away
+  clipOrphaned(map);
+} //end function
+
+// Traverse a location completely
+function traverse(map,locStats,unmapped,x,y){
+  let newLoc = null; //we pull from unmapped
+
+  locStats.val=1; //set the current mas size to 1
+  map.setRoom({x,y,id: locStats.cur});
+  traverseLook(map,unmapped,x,y);
+  while(unmapped.length>0){
+    newLoc=unmapped.pop();
+    traverseLook(map,unmapped,newLoc.x,newLoc.y);
+    map.setRoom({x: newLoc.x,y: newLoc.y,id: locStats.cur});
+    locStats.val++;
+    if(locStats.val>locStats.max){
+      locStats.max=locStats.val;
+      locStats.num=locStats.cur;
+    } //end manage maximum mass
+  } //end while
+} //end traverse()
+
+//look around at location and push unmapped nodes to stack
+function traverseLook(map,unmapped,x,y){
+  if(x>0&&map.isWalkableOrEmpty({x: x-1,y})&&!map.getRoom({x: x-1,y})){
+    unmapped.push({x: x-1, y});
+    map.setRoom({x: x-1,y,id: -1});
+  } //end if
+  if(y>0&&map.isWalkableOrEmpty({x,y: y-1})&&!map.getRoom({x,y: y-1})){
+    unmapped.push({x,y: y-1});
+    map.setRoom({x,y: y-1,id: -1});
+  } //end if
+  if(x<map.width&&map.isWalkableOrEmpty({x: x+1,y})&&!map.getRoom({x: x+1,y})){
+    unmapped.push({x: x+1, y});
+    map.setRoom({x: x+1,y,id: -1});
+  } //end if
+  if(y<map.height&&map.isWalkableOrEmpty({x,y: y+1})&&!map.getRoom({x,y: y+1})){
+    unmapped.push({x,y: y+1});
+    map.setRoom({x,y: y+1,id: -1});
+  } //end if
+} //end traverseLook()
+
+// Remove orphaned floors by iterating through all sectors
+// and each time we find a floor we traverse from that section.
+// the largest section traversed is what we keep
+function clipOrphaned(map){
+  let locStats = {val: 0,cur: 0,num: 0,max: 0},
+      unmapped = [];
+
+  map.sectors.forEach((row,y)=>{
+    row.forEach((sector,x)=>{
+      if(sector.isWalkableOrEmpty()&&!sector.roomNumber){
+        locStats.cur++;
+        traverse(map,locStats,unmapped,x,y);
+      } //end if
+    });
+  });
+  map.sectors.forEach(row=>{
+    row.forEach(sector=>{
+      if(sector.isWalkableOrEmpty()&&sector.roomNumber!==locStats.num){
+        sector.setWall();
+      }else if(sector.isWalkableOrEmpty()&&!sector.isWater()){
+        sector.setFloor();
+      } //end if
+    });
+  });
+} //end clipOrphaned()
