cc7e5d8013253bb891fd5860cf02fe0e6d461318
Nathaniel Inman <nate@theoestudio.com>
2018-08-22 11:17:39 -0500

    converted mesa to latest Map.js

--- mapGeneration/mesa/src/app.js
+++ mapGeneration/mesa/src/app.js
@@ -2,8 +2,8 @@ import {Easel} from 'ion-cloud';
 import {mesa} from './mesaGeneration';
 import {Map} from './Map';
 
-let noscript = document.querySelector('noscript'),
-    easel = new Easel();
+const noscript = document.querySelector('noscript'),
+      easel = new Easel();
 
 if(!easel.activated){
   noscript.innerHTML = `
@@ -14,32 +14,36 @@ if(!easel.activated){
     <span style="color:red;"><br/>Canvas isn't supported in your browser.</span>
   </p>`;
 }else{
-  let map = new Map(50,50);
+  const map = new Map(50,50);
 
   mesa(map); //perform pigeon hole generation
   easel.onDraw = function(){
-    let rh = easel.viewport.h/map.height, rw = easel.viewport.w/map.width;
+    const rh = easel.viewport.h/map.height, rw = easel.viewport.w/map.width;
 
     map.sectors.forEach((row,y)=>{
       row.forEach((sector,x)=>{
         if(sector.isEmpty()){
           easel.ctx.fillStyle='#000';
-        }else if(sector.isWall()){
-          easel.ctx.fillStyle='#333';
-        }else if(sector.isObstruction()){
-          easel.ctx.fillStyle='#888';
-        }else if(sector.isDoor()){
-          easel.ctx.fillStyle='#b94';
-        }else if(sector.isCorridor()){
-          easel.ctx.fillStyle='#774';
         }else if(sector.isRemoved()){
           easel.ctx.fillStyle='#833';
+        }else if(sector.isDoor()){
+          easel.ctx.fillStyle='#b94';
+        }else if(sector.isWallSpecial()){
+          easel.ctx.fillStyle='#445';
+        }else if(sector.isWall()){
+          easel.ctx.fillStyle='#334';
         }else if(sector.isWaterSpecial()){
           easel.ctx.fillStyle='#339';
         }else if(sector.isWater()){
           easel.ctx.fillStyle='#33b';
-        }else{ //floor
-          easel.ctx.fillStyle='#383';
+        }else if(sector.isFloorSpecial()){
+          easel.ctx.fillStyle='#563';
+        }else if(sector.isFloor()){
+          easel.ctx.fillStyle='#373';
+        }else if(sector.isVoid()){
+          easel.ctx.fillStyle='#111';
+        }else{ //unknown
+          easel.ctx.fillStyle='#f00';
         } //end if
 
         // the -0.4 & +0.8 is to remove sub-pixel issues
