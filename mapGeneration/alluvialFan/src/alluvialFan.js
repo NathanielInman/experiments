b5ea670ccd32325165accc8fde91b5e62671477c
Nathaniel Inman <nate@theoestudio.com>
2018-08-23 16:23:20 -0500

    mostly finished alluvial fan minus some inconsistencies with water outlet.

--- mapGeneration/alluvialFan/src/alluvialFan.js
+++ mapGeneration/alluvialFan/src/alluvialFan.js
@@ -26,7 +26,7 @@ export function alluvialFan(map){
         row.forEach(sector=>{
           const n = (1+map.noise.simplex2(sector.x/map.width,sector.y/map.height))/2;
 
-          if(n<0.6){
+          if(n<0.5){
             sector.setWall();
             if(
               sector.x===0||sector.x===map.width-1||
@@ -34,8 +34,12 @@ export function alluvialFan(map){
             ){
               viableStartSectors.push(sector);
             } //end if
+          }else if(n<0.53){
+            sector.setFloor();
+          }else if(n<0.55&&Math.random()<0.2){
+            sector.setFloor();
           }else{
-            sector.setWater();
+            sector.setWaterSpecial();
             if(
               sector.x>alluvialDistance&&sector.x<map.width-alluvialDistance&&
               sector.y>alluvialDistance&&sector.y<map.height-alluvialDistance
@@ -47,7 +51,6 @@ export function alluvialFan(map){
       });
     }while(!viableStartSectors.length||!viableEndSectors.length)
 
-    console.log(viableStartSectors.length,viableEndSectors.length);
     // now get a drunken path between a random start and end sector
 
     // we continue looping until we find a stream that outlets far enough
@@ -72,10 +75,18 @@ export function alluvialFan(map){
         x2: startSector.x, y2: startSector.y,
         draw(sector){
           if(!terminateEarly){
-            riverSectors.push(sector);
-            if(sector.isWater()){
+            const count = map.getNeighbors({
+              x: sector.x, y: sector.y,self: true,
+              test(sector){
+                return sector.isWater();
+              }
+            }).length;
+
+            if(count){
               terminalSector = sector;
               terminateEarly = true;
+            }else{
+              riverSectors.push(sector);
             } //end if
           } //end if
         }
@@ -117,18 +128,68 @@ export function alluvialFan(map){
       foci = (sa1*sa2)/Math.sqrt(p1+p2);
       if(hypotenuse<(foci/4*3)&&map.isInbounds({x,y})&&map.isWater({x,y})){
         map.setWaterSpecial({x,y});
+      }else if(hypotenuse<(foci/4*3)&&map.isInbounds({x,y})){
+        map.setWater({x,y});
       } //end if
       if(
-        hypotenuse<foci&&Math.random()<0.7&&
+        hypotenuse<foci&&Math.random()<0.85&&
         map.isInbounds({x,y})&&map.isWater({x,y})||
         hypotenuse<foci&&!endTerminalSector&&
         map.isInbounds({x,y})&&map.isWater({x,y})
       ){
         endTerminalSector = map.getSector({x,y});
-        endTerminalSector.setWaterSpecial();
+        endTerminalSector.setWater();
       } //end if
+      if(hypotenuse<foci&&map.isInbounds({x,y})){
+        map.setWater();
+      } //end if
+      if(isNaN(foci)) map.setWater({x,y}); //center of circle
     } //end for
   } //end for
-  riverSectors.forEach(sector=> sector.setWater());
-  console.log('river sectors length',riverSectors.length,startSector,terminalSector);
+  const finishedRiverSectors = [];
+
+  riverSectors.forEach(sector=>{
+    map.getNeighbors({
+      x: sector.x, y: sector.y, self: true,
+      test(sector){
+        const count = map.getNeighbors({
+          x: sector.x, y: sector.y, self: true,
+          test(sector){
+            return sector.isWater();
+          }
+        }).length;
+
+        return !count;
+      }
+    }).forEach(sector=> Math.random()<0.5?finishedRiverSectors.push(sector):null);
+  });
+  finishedRiverSectors.forEach((sector,i)=>{
+    sector.setWater();
+    if(i===finishedRiverSectors.length-1){
+      map.getNeighbors({
+        x: sector.x,y: sector.y, self: true
+      }).forEach(sector=> sector.setWater());
+    }
+  });
+
+  const d = Math.random()<0.5, //noise direction
+        h = d?7:10, //horizontal
+        v = d?10:7; //vertical
+
+  map.noise = new Noise(Math.random());
+  map.sectors.forEach(row=>{
+    row.forEach(sector=>{
+      const n = (1+map.noise.simplex2(sector.x/map.width*h,sector.y/map.height*v))/2;
+
+      if(n<0.6&&sector.isWall()&&Math.random()<0.4){
+        sector.setFloor();
+      }else if(n<0.7&&Math.random()<0.4&&sector.isWall()){
+        sector.setFloorSpecial();
+      }else if(n<0.7&&sector.isWall()){
+        sector.setFloor();
+      }else if(n<0.8&&sector.isWall()){
+        sector.setFloorSpecial();
+      } //end if
+    });
+  });
 } //end function
