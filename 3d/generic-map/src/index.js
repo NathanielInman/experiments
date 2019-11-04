472814ec07b8b9717effc8e44285d5a79f0ca05c
Nathaniel Inman <nate@theoestudio.com>
2019-11-04 16:09:33 -0600

    at a basic level now supporting all sector types in generic-map demo

--- 3d/generic-map/src/index.js
+++ 3d/generic-map/src/index.js
@@ -35,41 +35,71 @@ function main(){
 function initialize(){
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
-  const light = new THREE.PointLight(0xFFFFFF,2,300,10);
+  const light = new THREE.PointLight(0xFFFFFF,2,1300,10);
 
   light.castShadow = true;
   camera.add(light);
   controls = new Controls(renderer,camera);
   scene.add(controls.pointerLock.getObject());
   const material = new THREE.MeshPhysicalMaterial({color: 0x00FFFF,flatShading: true}),
-        geometry = new THREE.BoxBufferGeometry(sectorSize,sectorHeight,sectorSize);
+        geometryWall = new THREE.BoxBufferGeometry(sectorSize,sectorHeight,sectorSize),
+        geometryFloor = new THREE.BoxBufferGeometry(sectorSize,2,sectorSize);
 
   let playerPlaced = false;
 
   map.sectors.forEach((row,y)=>{
     row.forEach((sector,x)=>{
-      if(!sector.isWalkable()){
-        const entity = new THREE.Mesh(geometry,material);
+      let material, entity;
 
+      if(sector.isEmpty()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x000000,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
+      }else if(sector.isRemoved()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x803030,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
+      }else if(sector.isDoor()){
+        material = new THREE.MeshPhysicalMaterial({color: 0xb09040,flatShading: true});
+        entity = new THREE.Mesh(geometryWall,material);
         entity.castShadow = true;
-        entity.receiveShadow = true;
-        entity.position.set(x*sectorSize,0,y*sectorSize);
-        scene.add(entity);
-      }else{
-        const geometry = new THREE.BoxBufferGeometry(sectorSize,2,sectorSize),
-              entity = new THREE.Mesh(geometry,material);
-
-        entity.castShadow = false;
-        entity.receiveShadow = true;
-        entity.position.set(x*sectorSize,0,y*sectorSize);
-        scene.add(entity);
+      }else if(sector.isWallSpecial()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x404050,flatShading: true});
+        entity = new THREE.Mesh(geometryWall,material);
+        entity.castShadow = true;
+      }else if(sector.isWall()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x303040,flatShading: true});
+        entity = new THREE.Mesh(geometryWall,material);
+        entity.castShadow = true;
+      }else if(sector.isWaterSpecial()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x303090,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
+      }else if(sector.isWater()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x3030b0,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
+      }else if(sector.isFloorSpecial()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x506030,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
         if(!playerPlaced){
           controls.pointerLock.getObject().position.x = x*sectorSize;
           controls.pointerLock.getObject().position.z = y*sectorSize;
           playerPlaced = true;
           console.log('Player placed @',{x,y});
         } //end if
+      }else if(sector.isFloor()){
+        material = new THREE.MeshPhysicalMaterial({color: 0x307030,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
+        if(!playerPlaced){
+          controls.pointerLock.getObject().position.x = x*sectorSize;
+          controls.pointerLock.getObject().position.z = y*sectorSize;
+          playerPlaced = true;
+          console.log('Player placed @',{x,y});
+        } //end if
+      }else{
+        material = new THREE.MeshPhysicalMaterial({color: 0xf00000,flatShading: true});
+        entity = new THREE.Mesh(geometryFloor,material);
       } //end if
+      entity.receiveShadow = true;
+      entity.position.set(x*sectorSize,0,y*sectorSize);
+      scene.add(entity);
     });
   })
   renderer.setSize(window.innerWidth,window.innerHeight);
