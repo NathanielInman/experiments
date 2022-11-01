59000762364a459249ea8e676833382e1a7edff8
Nathaniel Inman <nate@theoestudio.com>
2022-11-01 16:44:12 -0500

    updated terrain-morph experiment

--- 3d/terrain-morph/src/PointerLockControls.js
+++ 3d/terrain-morph/src/PointerLockControls.js
@@ -1,73 +1,51 @@
 import * as THREE from 'three/build/three';
 
-export function loadPointerLockControls(){
-  /**
-   * @author mrdoob / http://mrdoob.com/
-   */
+const PI_2 = Math.PI / 2;
 
-  THREE.PointerLockControls = function ( camera ) {
+export class PointerLockControls {
+  constructor (camera) {
+    camera.rotation.set(0, 0, 0);
 
-    var scope = this;
-
-    camera.rotation.set( 0, 0, 0 );
-
-    var pitchObject = new THREE.Object3D();
-    pitchObject.add( camera );
-
-    var yawObject = new THREE.Object3D();
-    yawObject.position.y = 10;
-    yawObject.add( pitchObject );
-
-    var PI_2 = Math.PI / 2;
-
-    var onMouseMove = function ( event ) {
-
-      if ( scope.enabled === false ) return;
-
-      var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-      var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-
-      yawObject.rotation.y -= movementX * 0.002;
-      pitchObject.rotation.x -= movementY * 0.002;
-
-      pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
-
-    };
-
-    this.dispose = function() {
-
-      document.removeEventListener( 'mousemove', onMouseMove, false );
-
-    };
-
-    document.addEventListener( 'mousemove', onMouseMove, false );
-
+    this.pitchObject = new THREE.Object3D();
+    this.pitchObject.add(camera);
+    this.yawObject = new THREE.Object3D();
+    this.yawObject.position.y = 10;
+    this.yawObject.add(this.pitchObject);
     this.enabled = false;
 
-    this.getObject = function () {
+    const onMouseMove = event => {
+      if (this.enabled === false) return;
+      const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+      const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
-      return yawObject;
+      this.yawObject.rotation.y -= movementX * 0.002;
+      this.pitchObject.rotation.x -= movementY * 0.002;
 
+      this.pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, this.pitchObject.rotation.x));
     };
 
-    this.getDirection = function() {
+    this.dispose = function () {
+      document.removeEventListener('mousemove', onMouseMove, false);
+    };
 
-      // assumes the camera itself is not rotated
+    document.addEventListener('mousemove', onMouseMove, false);
+  }
 
-      var direction = new THREE.Vector3( 0, 0, - 1 );
-      var rotation = new THREE.Euler( 0, 0, 0, "XZY" );
+  getObject () {
+    return this.yawObject;
+  }
 
-      return function( v ) {
+  getDirection () {
+    // assumes the camera itself is not rotated
+    const direction = new THREE.Vector3(0, 0, -1);
+    const rotation = new THREE.Euler(0, 0, 0, 'XZY');
 
-        rotation.set( pitchObject.rotation.x, yawObject.rotation.y, 0 );
+    return v => {
+      rotation.set(this.pitchObject.rotation.x, this.yawObject.rotation.y, 0);
 
-        v.copy( direction ).applyEuler( rotation );
+      v.copy(direction).applyEuler(rotation);
 
-        return v;
-
-      };
-
-    }();
-
-  };
-} //end loadOrbitControls()
+      return v;
+    };
+  }
+}
--- 3d/terrain-morph/src/index.js
+++ 3d/terrain-morph/src/index.js
@@ -1,256 +1,263 @@
 import './index.styl';
-import {Map,Sector,maps} from '@ion-cloud/core';
+import { Map, Sector, maps } from '@ion-cloud/core';
 import * as StackBlur from 'stackblur-canvas';
 import * as THREE from 'three/build/three';
-import {loadPointerLockControls} from './PointerLockControls';
+import { PointerLockControls } from './PointerLockControls';
 
 const mapSize = 100;
 
 // Start by generating the map itself
-const map = new Map({width:mapSize,height:mapSize}),
-      binarySpacePartitioning = maps.find(map=> map.name==='caldera');
+const map = new Map({ width: mapSize, height: mapSize });
+const binarySpacePartitioning = maps.find(map => map.name === 'caldera');
 
-for(let y=0;y<map.height;y++){
-  map.sectors[y]=[];
-  for(let x=0;x<map.width;x++){
-    map.sectors[y][x]=new Sector({x,y,map});
-  } //end for
-} //end for
-binarySpacePartitioning.generator({map});
-let controlsEnabled=false, // Assume controls disabled until user allows
-    camera,scene,mesh,controls,mouseX=0,mouseY=0,
-    initialized = false,
-    moveForward = false,
-    moveBackward = false,
-    moveLeftward = false,
-    moveRightward = false,
-    renderer = new THREE.WebGLRenderer({antialias: true}),
-    instructions = document.querySelector('#instructions'),
-    prevTime = performance.now(), //used to determine moving velocity
-    velocity = new THREE.Vector3(), //helps with player movement
-    playerPlaced = false, //keep track of whether player was placed on map
-    raycaster, //make sure we can't walk into walls
-    onKeyDown = e=>{
-      if(e.keyCode===38||e.keyCode===87){ //up / w
-        moveForward = true;
-      }else if(e.keyCode===37||e.keyCode===65){ //left / a
-        moveLeftward = true;
-      }else if(e.keyCode===40||e.keyCode===83){ //down  s
-        moveBackward = true;
-      }else if(e.keyCode===39||e.keyCode===68){ //right / d
-        moveRightward = true;
-      }else if(e.keyCode===32){ //space
-      } //end if
-    },
-    onKeyUp = e=>{
-      if(e.keyCode===38||e.keyCode===87){ //up / w
-        moveForward = false;
-      }else if(e.keyCode===37||e.keyCode===65){ //left / a
-        moveLeftward = false;
-      }else if(e.keyCode===40||e.keyCode===83){ //down  s
-        moveBackward = false;
-      }else if(e.keyCode===39||e.keyCode===68){ //right / d
-        moveRightward = false;
-      }else if(e.keyCode===32){ //space
-      } //end if
-    };
+for (let y = 0; y < map.height; y++) {
+  map.sectors[y] = [];
+  for (let x = 0; x < map.width; x++) {
+    map.sectors[y][x] = new Sector({ x, y, map });
+  } // end for
+} // end for
+binarySpacePartitioning.generator({ map });
+let controlsEnabled = false; // Assume controls disabled until user allows
+let camera; let scene; let mesh; let controls;
+let initialized = false;
+let moveForward = false;
+let moveBackward = false;
+let moveLeftward = false;
+let moveRightward = false;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const instructions = document.querySelector('#instructions');
+let prevTime = performance.now(); // used to determine moving velocity
+const velocity = new THREE.Vector3(); // helps with player movement
+const onKeyDown = e => {
+  if (e.keyCode === 38 || e.keyCode === 87) { // up / w
+    moveForward = true;
+  } else if (e.keyCode === 37 || e.keyCode === 65) { // left / a
+    moveLeftward = true;
+  } else if (e.keyCode === 40 || e.keyCode === 83) { // down  s
+    moveBackward = true;
+  } else if (e.keyCode === 39 || e.keyCode === 68) { // right / d
+    moveRightward = true;
+  } else if (e.keyCode === 32) { // space
+  } // end if
+};
+const onKeyUp = e => {
+  if (e.keyCode === 38 || e.keyCode === 87) { // up / w
+    moveForward = false;
+  } else if (e.keyCode === 37 || e.keyCode === 65) { // left / a
+    moveLeftward = false;
+  } else if (e.keyCode === 40 || e.keyCode === 83) { // down  s
+    moveBackward = false;
+  } else if (e.keyCode === 39 || e.keyCode === 68) { // right / d
+    moveRightward = false;
+  } else if (e.keyCode === 32) { // space
+  } // end if
+};
 
 instructions.style.display = 'flex';
-acquirePointerLock(); //ask the user to user pointer lock
-document.addEventListener('keydown',onKeyDown,false);
-document.addEventListener('keyup',onKeyUp,false);
-loadPointerLockControls();
-window.addEventListener('resize',onWindowResize,true);
+acquirePointerLock(); // ask the user to user pointer lock
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
+window.addEventListener('resize', onWindowResize, true);
 main();
 
-function main(){
-  let time = performance.now(),
-      timeDelta = (time-prevTime)/1000,
-      forwardWalkable = true,
-      backwardWalkable = true,
-      leftwardWalkable = true,
-      rightwardWalkable = true;
+function main () {
+  const time = performance.now();
+  const timeDelta = (time - prevTime) / 1000;
 
-  if(!initialized) initialize();
-  if(controlsEnabled){
-    let geoSize = mesh.geometry.parameters.width,
-        controlClone = controls.getObject().clone(),
-        px,py,position; //testing potential player x and y
+  if (!initialized) initialize();
+  if (controlsEnabled) {
+    const geoSize = mesh.geometry.parameters.width;
+    const controlClone = controls.getObject().clone();
 
     velocity.x -= velocity.x * 10.0 * timeDelta;
     velocity.z -= velocity.z * 10.0 * timeDelta;
     velocity.y -= 9.8 * 100.0 * timeDelta; // 100.0 = mass
-    if(moveLeftward) velocity.x -= 500.0 * timeDelta;
-    if(moveForward) velocity.z -= 500.0 * timeDelta;
-    if(moveBackward) velocity.z += 500.0 * timeDelta;
-    if(moveRightward) velocity.x += 500.0 * timeDelta;
-    controlClone.translateX(velocity.x*timeDelta);
-    controlClone.translateZ(velocity.z*timeDelta);
-    controlClone.translateY(velocity.y*timeDelta);
-    position = controlClone.position;
-    px = Math.floor((position.x+geoSize/2)/20+0.5);
-    py = Math.floor((position.z+geoSize/2)/20+0.5);
-    if(map.isWalkable({x:px,y:py})){
-      controls.getObject().translateX(velocity.x*timeDelta);
-      controls.getObject().translateZ(velocity.z*timeDelta);
-      controls.getObject().translateY(velocity.y*timeDelta);
-    } //end if
-    if(controls.getObject().position.y<10){
+    if (moveLeftward) velocity.x -= 500.0 * timeDelta;
+    if (moveForward) velocity.z -= 500.0 * timeDelta;
+    if (moveBackward) velocity.z += 500.0 * timeDelta;
+    if (moveRightward) velocity.x += 500.0 * timeDelta;
+    controlClone.translateX(velocity.x * timeDelta);
+    controlClone.translateZ(velocity.z * timeDelta);
+    controlClone.translateY(velocity.y * timeDelta);
+    const position = controlClone.position;
+    const px = Math.floor((position.x + geoSize / 2) / 20 + 0.5);
+    const py = Math.floor((position.z + geoSize / 2) / 20 + 0.5);
+    if (map.isWalkable({ x: px, y: py })) {
+      controls.getObject().translateX(velocity.x * timeDelta);
+      controls.getObject().translateZ(velocity.z * timeDelta);
+      controls.getObject().translateY(velocity.y * timeDelta);
+    } // end if
+    if (controls.getObject().position.y < 10) {
       velocity.y = 0;
       controls.getObject().position.y = 10;
-    } //end if
+    } // end if
     prevTime = time;
-  } //end if
-  renderer.render(scene,camera);
+  } // end if
+  renderer.render(scene, camera);
   requestAnimationFrame(main);
-} //end main()
+} // end main()
 
-function initialize(){
-  let texture = new THREE.CanvasTexture(generateTexture(2048,2048)),
-      material = new THREE.MeshLambertMaterial({
-        color: '#009696',
-        flatShading: true,
-        fog: true,
-        map: texture
-      }),
-      geometry = new THREE.PlaneGeometry(2000,2000,mapSize*2,mapSize*2),
-      lantern = new THREE.PointLight(0xFFFFFF,2,800),
-      floorShape = new THREE.Shape(),
-      floorShapeWidth = geometry.parameters.width/2/mapSize,
-      floorGeometry,
-      floors = new THREE.Group();
+function initialize () {
+  const texture = new THREE.CanvasTexture(generateTexture(2048, 2048));
+  const material = new THREE.MeshLambertMaterial({
+    color: '#009696',
+    flatShading: true,
+    fog: true,
+    map: texture
+  });
+  const geometry = new THREE.PlaneGeometry(2000, 2000, mapSize * 2, mapSize * 2);
+  const lantern = new THREE.PointLight(0xFFFFFF, 2, 800);
+  const floorShape = new THREE.Shape();
+  const floorShapeWidth = geometry.parameters.width / 2 / mapSize;
+  let floorGeometry;
+  const floors = new THREE.Group();
 
   initialized = true;
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.add(lantern);
-  controls = new THREE.PointerLockControls(camera);
+  controls = new PointerLockControls(camera);
   scene.add(controls.getObject());
-  geometry.rotateX(-Math.PI/2);
-  raycaster = new THREE.Raycaster(new THREE.Vector3(),new THREE.Vector3(0,-1,0),0,10);
-  floorShape.moveTo(0,0);
-  floorShape.lineTo(0,floorShapeWidth);
-  floorShape.lineTo(floorShapeWidth,floorShapeWidth);
-  floorShape.lineTo(floorShapeWidth,0);
-  floorShape.lineTo(0,0);
-  floorGeometry = new THREE.ShapeBufferGeometry(floorShape);
-  console.log(geometry);
-  for(let i=0,x,y,mx,my,face,floor,mesh,gx,gz,gw;i<geometry.faces.length;i++){
-    face = geometry.faces[i].b;
-    gx = geometry.vertices[face].x;
-    gz = geometry.vertices[face].z;
-    gw = geometry.parameters.width/2;
-    x = gx+gw;
-    y = gz+gw;
-    mx = Math.floor(x/20);
-    my = Math.floor(y/20);
+  geometry.rotateX(-Math.PI / 2);
+  floorShape.moveTo(0, 0);
+  floorShape.lineTo(0, floorShapeWidth);
+  floorShape.lineTo(floorShapeWidth, floorShapeWidth);
+  floorShape.lineTo(floorShapeWidth, 0);
+  floorShape.lineTo(0, 0);
+  for (let i = 0, x, y, mx, my, face, mesh, gx, gz, gw; i < geometry.attributes.position.array.length; i+=9) {
+    // non-indexed is points * 3 = vertices * 3 = face
+    // points = x1,y1,z1,x2,y2,z2,x3,y2,z3
+    // vertices = [x1,y1,z1],[x2,y2,z2],[x3,y2,z3]
+    // faces = [[x1,y1,z1],[x2,y2,z2],[x3,y3,z3]]
+    const x1 = i;
+    const y1 = i+1;
+    const z1 = i+2;
+    const x2 = i+3;
+    const y2 = i+4;
+    const z2 = i+5;
+    const x3 = i+6;
+    const y3 = i+7;
+    const z3 = i+8;
+    const setPoint = (location, value) => geometry.attributes.position.array[location] = value;
+    const getPoint = (location) => geometry.attributes.position.array[location];
+
+    gx = getPoint(x1)
+    gz = getPoint(z1)
+    gw = geometry.parameters.width / 2;
+    x = gx + gw;
+    y = gz + gw;
+    mx = Math.floor(x / 20);
+    my = Math.floor(y / 20);
 
     // make sure the value is in-bound of the map itself or ignore it
-    if(mx>=mapSize||my>=mapSize||Math.ceil(x/20)>=mapSize||Math.ceil(y/20)>=mapSize) continue;
-    if(map.isWalkable({x:mx,y:my})){
-      geometry.vertices[face].y=0;
-      mesh = new THREE.Mesh(floorGeometry,new THREE.MeshStandardMaterial({color:0x005555,wireframe:true}));
-      mesh.position.set(gx,1,gz);
-      mesh.rotation.set(-Math.PI/2,0,0);
-      mesh.scale.set(0.9,0.9,1);
+    if (mx >= mapSize || my >= mapSize || Math.ceil(x / 20) >= mapSize || Math.ceil(y / 20) >= mapSize) continue;
+    if (map.isWalkable({ x: mx, y: my })) {
+      setPoint(y1, 0);setPoint(y2, 0);setPoint(y3, 0)
+      mesh = new THREE.Mesh(floorGeometry, new THREE.MeshStandardMaterial({ color: 0xff5555, wireframe: true }));
+      mesh.position.set(gx, 1, gz);
+      mesh.rotation.set(-Math.PI / 2, 0, 0);
+      mesh.scale.set(0.9, 0.9, 1);
       floors.add(mesh);
 
       // always update camera with latest floor so the last floor is the camera,
       // which will face the camera towards the scene as the center scene is origin
-      controls.getObject().position.x = geometry.vertices[face].x;
-      controls.getObject().position.z = geometry.vertices[face].z;
-    }else if(map.isWalkable({x:Math.ceil(x/20),y:Math.ceil(y/20)})){
-      geometry.vertices[face].y=0;
-      mesh = new THREE.Mesh(floorGeometry,new THREE.MeshStandardMaterial({color:0x005555,wireframe:true}));
-      mesh.position.set(gx,1,gz);
-      mesh.rotation.set(-Math.PI/2,0,0);
-      mesh.scale.set(0.9,0.9,1);
+      controls.getObject().position.x = getPoint(x1);
+      controls.getObject().position.z = getPoint(z1);
+    } else if (map.isWalkable({ x: Math.ceil(x / 20), y: Math.ceil(y / 20) })) {
+      setPoint(y1, 0);setPoint(y2,0);setPoint(y3,0);
+      mesh = new THREE.Mesh(floorGeometry, new THREE.MeshStandardMaterial({ color: 0xff5555, wireframe: true }));
+      mesh.position.set(gx, 1, gz);
+      mesh.rotation.set(-Math.PI / 2, 0, 0);
+      mesh.scale.set(0.9, 0.9, 1);
       floors.add(mesh);
-    }else{
-      geometry.vertices[face].y=20;
-    } //end if
-  } //end for
-  mesh = new THREE.Mesh(geometry,material);
+    } else {
+      setPoint(y1, 20);setPoint(y2,20);setPoint(y3,20);
+    } // end if
+    geometry.attributes.position.needsUpdate = true;
+  } // end for
+  mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
   scene.add(floors);
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
-} //end initialize()
+} // end initialize()
 
 // TODO: Repurpose this function for creating a texture height map so that
 // colors flow from dark at the top to light at the bottom where the user
 // stands
-function generateTexture(width,height){
-  let canvas, ctx, nx, ny;
+function generateTexture (width, height) {
+  let nx, ny;
 
-  canvas = document.createElement('canvas');
+  const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
-  ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
   ctx.fillStyle = '#000';
-  ctx.fillRect(0,0,width,height);
+  ctx.fillRect(0, 0, width, height);
   ctx.fillStyle = '#fff';
-  for(let y=0;y<height;y++){
-    for(let x=0;x<width;x++){
-      nx = Math.floor(x/width*mapSize+0.5);
-      ny = Math.floor(y/height*mapSize+0.5);
-      if(nx>=mapSize||ny>=mapSize) continue;
-      if(map.isWalkable({x:nx,y:ny})){
-        ctx.fillRect(x,y,1,1);
-      } //end if
-    } //end for
-  } //end for
-  StackBlur.canvasRGB(canvas,1,1,width,height,15);
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      nx = Math.floor(x / width * mapSize + 0.5);
+      ny = Math.floor(y / height * mapSize + 0.5);
+      if (nx >= mapSize || ny >= mapSize) continue;
+      if (map.isWalkable({ x: nx, y: ny })) {
+        ctx.fillRect(x, y, 1, 1);
+      } // end if
+    } // end for
+  } // end for
+  StackBlur.canvasRGB(canvas, 1, 1, width, height, 15);
   return canvas;
-} //end generateTexture()
+} // end generateTexture()
 
-function onWindowResize(){
-  let w = window.innerWidth,
-      h = window.innerHeight;
+function onWindowResize () {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
 
-  renderer.setSize(w,h);
-  camera.aspect = w/h;
+  renderer.setSize(w, h);
+  camera.aspect = w / h;
   camera.updateProjectionMatrix();
-} //end onWindowResize()
+} // end onWindowResize()
 
-function acquirePointerLock(){
-  let havePointerLock = 'pointerLockElement' in document ||
+function acquirePointerLock () {
+  const havePointerLock = 'pointerLockElement' in document ||
                         'mozPointerLockElement' in document ||
                         'webkitPointerLockElement' in document;
 
-  if(havePointerLock){
-    let element = document.body,
-        pointerlockchange = (event)=>{
-          if(document.pointerLockElement === element ||
+  if (havePointerLock) {
+    const element = document.body;
+    const pointerlockchange = (event) => {
+      if (document.pointerLockElement === element ||
              document.mozPointerLockElement === element ||
-             document.webkitPointerLockElement === element){
-            controlsEnabled = true;
-            controls.enabled = true;
-            instructions.style.display = 'none';
-          } else {
-            controls.enabled = false;
-            instructions.style.display = 'flex';
-          } //end if
-        },
-        pointerlockerror = (event) => instructions.style.display = '';
+             document.webkitPointerLockElement === element) {
+        controlsEnabled = true;
+        controls.enabled = true;
+        instructions.style.display = 'none';
+      } else {
+        controls.enabled = false;
+        instructions.style.display = 'flex';
+      } // end if
+    };
+
+    // eslint-disable-next-line no-return-assign
+    const pointerlockerror = (event) => instructions.style.display = '';
 
     // Hook pointer lock state change events
-    document.addEventListener('pointerlockchange',pointerlockchange,false);
-    document.addEventListener('mozpointerlockchange',pointerlockchange,false);
-    document.addEventListener('webkitpointerlockchange',pointerlockchange,false);
-    document.addEventListener('pointerlockerror',pointerlockerror,false);
-    document.addEventListener('mozpointerlockerror',pointerlockerror,false);
-    document.addEventListener('webkitpointerlockerror',pointerlockerror,false);
+    document.addEventListener('pointerlockchange', pointerlockchange, false);
+    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+    document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+    document.addEventListener('pointerlockerror', pointerlockerror, false);
+    document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+    document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
 
-    instructions.addEventListener('click',function(event){
+    instructions.addEventListener('click', function (event) {
       instructions.style.display = 'none';
 
       // Ask the browser to lock the pointer
       element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
       element.requestPointerLock();
-    },false);
-  }else{
+    }, false);
+  } else {
     instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-  } //end if
+  } // end if
 }
